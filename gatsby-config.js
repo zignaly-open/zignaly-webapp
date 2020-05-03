8fdb3db452c5bc36c78fa112e9e72d904404f73c
Pablo Cerda <pablo@citla.com>
2020-05-03 17:47:03 +0200

    ZIG-281: Basic Gatsby plugins configuration.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,31 @@
 module.exports = {
-    plugins: [`gatsby-plugin-sass`, `gatsby-plugin-react-helmet`]
-};
+    siteMetadata: {
+        title: `Zignaly Gatsby Starter`,
+        description: `Zignaly the copy-trader platform.`,
+    },
+    plugins: [
+        `gatsby-plugin-react-helmet`,
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `images`,
+                path: `${__dirname}/src/images`,
+            },
+        },
+        `gatsby-transformer-sharp`,
+        `gatsby-plugin-sharp`,
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: `gatsby-starter-default`,
+                short_name: `Zignaly`,
+                start_url: `/`,
+                background_color: `#663399`,
+                theme_color: `#663399`,
+                display: `minimal-ui`,
+                // This path is relative to the root of the site.
+                icon: `src/images/gatsby-icon.png`,
+            },
+        },
+    ]
+}
