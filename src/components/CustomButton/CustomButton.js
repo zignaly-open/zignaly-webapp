ce8f37e7a2e007deb0a235c4a35ccd795b9c93e6
Murad Malik <muradmalik7@gmail.com>
2020-05-06 18:43:26 +0500

    ZIG-217: header components designed and links added default user

--- src/components/CustomButton/CustomButton.js
+++ src/components/CustomButton/CustomButton.js
@@ -1,16 +1,32 @@
 import React, {Component} from 'react';
-import './CustomButton.sass';
+import style from './CustomButton.module.sass';
 import {Button, CircularProgress} from '@material-ui/core';
 
-class CustomButton extends Component{
-    render(){
-        const {loading, className, children, onClick, disabled} = this.props
-        return (
-            <Button onClick={onClick} disabled={disabled} className={"custom-button " + className}>
-                {loading ? <CircularProgress thickness={5} className="loader" /> :  children}
-            </Button>
-        )
+const CustomButton = (props) => {
+    const {loading, children, onClick, disabled, type} = props
+
+    const getType = () => {
+        switch(type){
+            case "green":
+                return style.green
+            case "purple":
+                return style.purple
+            case "red":
+                return style.red
+            case "pink":
+                return style.pink
+            case "blue":
+                return style.pink
+            default:
+                return style.purple
+        }
     }
+
+    return (
+        <Button onClick={onClick} disabled={disabled} className={[style.customButton, getType()].join(" ")}>
+            {loading ? <CircularProgress thickness={5} className="loader" /> :  children}
+        </Button>
+    )
 }
 
 export default CustomButton;
