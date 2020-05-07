0d8c5ee7d6c47c6a5c2d68a16bc743a26c6a67c0
Murad Malik <muradmalik7@gmail.com>
2020-05-07 17:04:58 +0500

    ZIG-219: dashboard component added for user who hasn't added any exchanges

--- src/layouts/dashboardLayout/withDashboardLayout.jsx
+++ src/layouts/dashboardLayout/withDashboardLayout.jsx
@@ -8,19 +8,34 @@ import DashboardHeader from "../../components/Dashboard/DashboardHeader";
 
 const withDashboardLayout = Component => {
     const WrapperComponent = props => {
+        const exchange = false
         return (
             <Box className="dashboardLayout" display="flex" flexDirection="column" justifyContent="flex-start" alignItems="flex-start">
-                <Box className="titleBox" display="flex" flexDirection="row" justifyContent="flex-start" alignItems="flex-end">
+                <Box className="titleBox" display="flex" flexDirection="row" justifyContent="flex-start" alignItems="center">
                     <span className="pageTitle">Dashboard</span>
-                    <span className="exchangeTitle">KuCion (Demo)</span>
+                    {exchange && <span className="exchangeTitle">KuCion (Demo)</span>}
                 </Box>
-                <DashboardHeader />
-                <Box className="pageContent">
-                    <Component {...props} />
-                </Box>
-                <Box className="faq">
-                    <FAQ />
-                </Box>
+                {exchange &&
+                    <React.Fragment>
+                        <DashboardHeader />
+                        <Box className="pageContent">
+                            <Component {...props} />
+                        </Box>
+                        <Box className="faq">
+                            <FAQ />
+                        </Box>
+                    </React.Fragment>
+                }
+                {!exchange &&
+                    <Box className="noExchangeBox" display="flex" flexDirection="column" justifyContent="flex-start">
+                        <span className="title">
+                            Go to <b>My Exchange Accounts</b> in the menu at the top-right to set up your exchange
+                        </span>
+                        <span className="text">
+                            To be able to trade on Zignaly you need to connect or create at least one exchange accounts like <b>Binance</b> or <b>KuCoin</b>.
+                        </span>
+                    </Box>
+                }
             </Box>
         );
     };
