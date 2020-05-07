57f787bd28eea4819ea43376a3ee35c85b88f828
Murad Malik <muradmalik7@gmail.com>
2020-05-07 18:07:23 +0500

    ZIG-222: settings icons added to positions tabs

--- src/components/Dashboard/PositionsTabs/PositionsTabs.js
+++ src/components/Dashboard/PositionsTabs/PositionsTabs.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import './PositionsTabs.sass';
 import { Box, Tab, Tabs } from '@material-ui/core';
+import SettingsIcon from '../../../images/dashboard/settings.svg';
+import BorderIcon from '../../../images/dashboard/border.svg';
 
 const LoginTabs = (props) => {
     const [tabValue, setTabValue] = useState(0)
@@ -12,9 +14,15 @@ const LoginTabs = (props) => {
     return(
         <Box bgcolor="grid.main" className="positionsTabs">
             <Tabs value={tabValue} onChange={changeTab} classes={{indicator: 'indicator', flexContainer: 'container'}} className="tabs-menu">
-                <Tab label="open positions" classes={{selected: 'selected'}} />
-                <Tab label="closed positions" classes={{selected: 'selected'}} />
-                <Tab label="logs" classes={{selected: 'selected'}} />
+                <Box>
+                    <Tab label="open positions" classes={{selected: 'selected'}} />
+                    <Tab label="closed positions" classes={{selected: 'selected'}} />
+                    <Tab label="logs" classes={{selected: 'selected'}} />
+                </Box>
+                <Box className="settings" display="flex" flexDirection="row" justifyContent="space-between" alignItems="center">
+                    <img src={BorderIcon} alt="zignaly" className="icon" />
+                    <img src={SettingsIcon} alt="zignaly" className="icon" />
+                </Box>
             </Tabs>
             {tabValue === 0 &&
                 <Box className="section">
