eb84ae9b46f422967f7289474e1277118c910e98
Pablo Cerda <pablo@citla.com>
2020-05-13 22:37:20 +0200

    ZIG-222: Define UserCreatePayload typedef and use at userCreate.

--- src/services/tradeApiClient.js
+++ src/services/tradeApiClient.js
@@ -1,60 +1,68 @@
 // @flow
-import fetch from "cross-fetch";
+import fetch from "cross-fetch"
 
 // Trade API client service, provides integration to API endpoints.
 class TradeApiClient {
-   constructor() {
-       this.baseUrl = process.env.TRADEAPI_URL;
+  constructor() {
+    this.baseUrl = process.env.TRADEAPI_URL
 
-        if (!TradeApiClient.instance) {
-            TradeApiClient.instance = this;
-        }
+    if (!TradeApiClient.instance) {
+      TradeApiClient.instance = this
+    }
 
-        return TradeApiClient;
-   }
+    return TradeApiClient
+  }
 
-   /**
-    * Login a user in Trade API.
-    *
-    * @param {UserLoginPayload} payload
-    * @returns string The session token.
-    * @memberof TradeApiClient
-    */
-   userLogin(payload) {
+  /**
+   * Login a user in Trade API.
+   *
+   * @param {UserLoginPayload} payload
+   * @returns string The session token.
+   * @memberof TradeApiClient
+   */
+  userLogin(payload) {}
 
-   }
+  userLogout() {}
 
-   userLogout() {
+  /**
+   * @typedef {Object} UserCreatePayload
+   * @property {string} firstName
+   * @property {string} email
+   * @property {string} passwrod
+   */
 
-   }
-
-   async userCreate(userCreatePayload) {
-    const apiBaseUrl = 'http://api.zignaly.lndo.site/';
-    const endpointPath = 'fe/api.php?action=signup';
-    const requestUrl = apiBaseUrl + endpointPath;
+  /**
+   * Create user at Zignaly Trade API.
+   *
+   * @param {UserCreatePayload} UserCreatePayload
+   * @returns
+   * @memberof TradeApiClient
+   */
+  async userCreate(UserCreatePayload) {
+    const apiBaseUrl = "http://api.zignaly.lndo.site/"
+    const endpointPath = "fe/api.php?action=signup"
+    const requestUrl = apiBaseUrl + endpointPath
     const options = {
-        method: 'POST',
-        body: JSON.stringify(userCreatePayload)
+      method: "POST",
+      body: JSON.stringify(UserCreatePayload),
     }
 
     try {
-      const response = await fetch(requestUrl, options);
+      const response = await fetch(requestUrl, options)
       if (response.status === 200) {
-        return await response.json();
+        return await response.json()
       }
 
-      throw new Error("User creation failed.");
+      throw new Error("User creation failed.")
     } catch (e) {
-      console.error(e);
+      console.error(e)
     }
-   }
+  }
 
-   openPositionsGet(token, positionStatus) {
-
-   }
+  openPositionsGet(token, positionStatus) {}
 }
 
-const client = new TradeApiClient();
-Object.freeze(client);
+const client = new TradeApiClient()
+Object.freeze(client)
 
-export default client.instance;
+export default client.instance
