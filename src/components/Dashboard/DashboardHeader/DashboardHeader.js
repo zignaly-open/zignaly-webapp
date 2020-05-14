33a75e80f01146869078e4a5d30befba8a11a98c
Murad Malik <muradmalik7@gmail.com>
2020-05-15 02:36:01 +0500

    ZIG-221: translations added for users with exchange connected

--- src/components/Dashboard/DashboardHeader/DashboardHeader.js
+++ src/components/Dashboard/DashboardHeader/DashboardHeader.js
@@ -2,18 +2,19 @@ import React from 'react';
 import './DashboardHeader.sass';
 import { Box } from '@material-ui/core';
 import Link from '../../LocalizedLink';
+import {FormattedMessage} from 'react-intl';
 
 const DashboardHeader = () => {
     return (
         <Box display="flex" flexDirection="row" justifyContent="flex-start" alignItems="center" className="dashboardHeader hideScroll">
             <Link to="/dashboard/positions" className="dashboardLink" activeClassName="active">
-                Positions
+                <FormattedMessage id="dashboard.positions" />
             </Link>
             <Link to="/dashboard/balance" className="dashboardLink" activeClassName="active">
-                Balance
+                <FormattedMessage id="dashboard.balance" />
             </Link>
             <Link to="/dashboard/connectedTraders" className="dashboardLink" activeClassName="active">
-                connected traders
+                <FormattedMessage id="dashboard.traders" />
             </Link>
         </Box>
     )
