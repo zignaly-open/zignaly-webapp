b428b35200d5d2842ca737685922f67f2a9d9b3d
Pablo Cerda <pablo@citla.com>
2020-05-14 23:26:12 +0200

    ZIG-222: Integrate Trade API getOpenPositions into PositionTable.

--- src/components/Dashboard/PositionsTable/PositionsTable.js
+++ src/components/Dashboard/PositionsTable/PositionsTable.js
@@ -1,50 +1,141 @@
-import React from 'react';
-import './PositionsTable.sass';
-import {Box, Table, TableHead, TableBody, TableRow, TableCell} from '@material-ui/core';
+import React, { useEffect, useState } from "react"
+import "./PositionsTable.sass"
+import {
+  Box,
+  Table,
+  TableHead,
+  TableBody,
+  TableRow,
+  TableCell,
+} from "@material-ui/core"
+import tradeApi from "../../../services/tradeApiClient"
+import * as TradeApiTypes from "../../../services/tradeApiClient.types"
 
-const PositionsTable = (props) => {
-    const rows = [1,2,3,4,5,6];
+const PositionsTable = props => {
+  const [positions, setPositions] = useState([])
+  const authenticateUser = async () => {
+    const loginPayload = {
+      email: "mailxuftg1pxzk@example.test",
+      password: "abracadabra",
+    }
 
-    return(
-        <Box className="positionsTable" display="flex" flexDirection="column" justifyContent="center" alignItems="center">
-            <Box className="tableBox" display="flex" flexDirection="row" justifyContent="center">
-                <Table className="table">
-                    <TableHead className="head">
-                        <TableRow className="row">
-                            <TableCell align="left" className="cell">Time</TableCell>
-                            <TableCell align="left" className="cell">Type</TableCell>
-                            <TableCell align="left" className="cell">Pair</TableCell>
-                            <TableCell align="left" className="cell">Trader</TableCell>
-                            <TableCell align="left" className="cell">Invested</TableCell>
-                            <TableCell align="left" className="cell">Entry Price</TableCell>
-                            <TableCell align="left"className="cell" >Current Price</TableCell>
-                            <TableCell align="left" className="cell">P/L %</TableCell>
-                            <TableCell align="left" className="cell">P/L #</TableCell>
-                            <TableCell align="left" className="cell">Net Profit</TableCell>
-                            <TableCell align="left" className="cell">Risk</TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody className="body">
-                    {rows.map((row) => (
-                        <TableRow key={row} className="row">
-                            <TableCell align="left" className="cell">{"time"}</TableCell>
-                            <TableCell align="left" className="cell">{"data"}</TableCell>
-                            <TableCell align="left" className="cell">{"data"}</TableCell>
-                            <TableCell align="left" className="cell">{"data"}</TableCell>
-                            <TableCell align="left" className="cell">{"time"}</TableCell>
-                            <TableCell align="left" className="cell">{"data"}</TableCell>
-                            <TableCell align="left" className="cell">{"data"}</TableCell>
-                            <TableCell align="left" className="cell">{"data"}</TableCell>
-                            <TableCell align="left" className="cell">{"time"}</TableCell>
-                            <TableCell align="left" className="cell">{"data"}</TableCell>
-                            <TableCell align="left" className="cell">{"time"}</TableCell>
-                        </TableRow>
-                    ))}
-                    </TableBody>
-                </Table>
-            </Box>
-        </Box>
-    )
+    return await tradeApi.userLogin(loginPayload)
+  }
+
+  const loadPositions = async () => {
+    const userEntity = await authenticateUser()
+    const sessionPayload = {
+      token: userEntity.token,
+    }
+
+    const positions = await tradeApi.openPositionsGet(sessionPayload)
+    setPositions(positions)
+  }
+
+  useEffect(() => {
+    loadPositions()
+  }, [])
+
+  /**
+   * @type {TradeApiTypes.UserPositionsCollection} positionsCollection
+   */
+  const positionsCollection = positions
+
+  return (
+    <Box
+      className="positionsTable"
+      display="flex"
+      flexDirection="column"
+      justifyContent="center"
+      alignItems="center"
+    >
+      <Box
+        className="tableBox"
+        display="flex"
+        flexDirection="row"
+        justifyContent="center"
+      >
+        <Table className="table">
+          <TableHead className="head">
+            <TableRow className="row">
+              <TableCell align="left" className="cell">
+                Time
+              </TableCell>
+              <TableCell align="left" className="cell">
+                Type
+              </TableCell>
+              <TableCell align="left" className="cell">
+                Pair
+              </TableCell>
+              <TableCell align="left" className="cell">
+                Trader
+              </TableCell>
+              <TableCell align="left" className="cell">
+                Invested
+              </TableCell>
+              <TableCell align="left" className="cell">
+                Entry Price
+              </TableCell>
+              <TableCell align="left" className="cell">
+                Current Price
+              </TableCell>
+              <TableCell align="left" className="cell">
+                P/L %
+              </TableCell>
+              <TableCell align="left" className="cell">
+                P/L #
+              </TableCell>
+              <TableCell align="left" className="cell">
+                Net Profit
+              </TableCell>
+              <TableCell align="left" className="cell">
+                Risk
+              </TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody className="body">
+            {positionsCollection.map(position => (
+              <TableRow key={position.positionId} className="row">
+                <TableCell align="left" className="cell">
+                  {position.openDate}
+                </TableCell>
+                <TableCell align="left" className="cell">
+                  {position.side}
+                </TableCell>
+                <TableCell align="left" className="cell">
+                  {"data"}
+                </TableCell>
+                <TableCell align="left" className="cell">
+                  {"data"}
+                </TableCell>
+                <TableCell align="left" className="cell">
+                  {"time"}
+                </TableCell>
+                <TableCell align="left" className="cell">
+                  {"data"}
+                </TableCell>
+                <TableCell align="left" className="cell">
+                  {"data"}
+                </TableCell>
+                <TableCell align="left" className="cell">
+                  {"data"}
+                </TableCell>
+                <TableCell align="left" className="cell">
+                  {"time"}
+                </TableCell>
+                <TableCell align="left" className="cell">
+                  {"data"}
+                </TableCell>
+                <TableCell align="left" className="cell">
+                  {"time"}
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </Box>
+    </Box>
+  )
 }
 
-export default PositionsTable;
+export default PositionsTable
