581bc6006a7ff8e991b42c5bb68f52e5a092d4bb
Murad Malik <muradmalik7@gmail.com>
2020-05-15 03:15:02 +0500

    ZIG-221: redirection added for no postions component

--- src/components/Dashboard/NoPositions/NoPositions.js
+++ src/components/Dashboard/NoPositions/NoPositions.js
@@ -3,12 +3,18 @@ import './NoPositions.sass';
 import { Box, Typography } from '@material-ui/core';
 import {FormattedMessage} from 'react-intl';
 import CustomButton from '../../CustomButton';
+import {navigate} from '@reach/router';
 
 const NoPositions = () => {
+
+    const redirect = () => {
+        navigate("/copyTraders")
+    }
+
     return (
         <Box display="flex" flexDirection="column" justifyContent="center" alignItems="center" className="noPositions">
             <Typography variant="h3"><FormattedMessage id="dashboard.positions.nopositions" /></Typography>
-            <CustomButton className="submit-btn"><FormattedMessage id="dashboard.browsetraders" /></CustomButton>
+            <CustomButton onClick={redirect} className="submit-btn"><FormattedMessage id="dashboard.browsetraders" /></CustomButton>
         </Box>
     )
 }
