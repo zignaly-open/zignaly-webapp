1f14a165a9d0d167a4f3d05f2c564e1f2cba6e2d
Murad Malik <muradmalik7@gmail.com>
2020-05-14 18:07:14 +0500

    ZIG-452: reset password form is noe refactored and react hook form added

--- src/components/Forms/ResetPasswordForm/ResetPasswordForm.js
+++ src/components/Forms/ResetPasswordForm/ResetPasswordForm.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, {Component, useState} from 'react';
 import './ResetPasswordForm.sass';
 import {Box, InputAdornment, IconButton, FormControl, OutlinedInput, Popper} from '@material-ui/core';
 import CustomButton from '../../CustomButton/CustomButton';
@@ -9,109 +9,71 @@ import { validatePassword } from '../../../helpers/validators';
 import Visibility from '@material-ui/icons/Visibility';
 import VisibilityOff from '@material-ui/icons/VisibilityOff';
 import PasswordStrength from '../../PasswordStrength';
+import {useForm} from 'react-hook-form';
 
-class TwoFAForm extends Component{
-    constructor(props){
-        super(props)
-        this.state = {
-            password: "",
-            passwordError: false,
-            repeatPassword: "",
-            repeatPasswordError: false,
-            loading: false,
-            showPassword: false,
-            showRepeatPassword: false,
-            strength: 0,
-            anchorEl: undefined,
+const ResetPasswordForm = (props) => {
+    const [anchorEl, setAnchorEl] = useState(undefined)
+    const [loading, setLoading] = useState(false)
+    const [passwordDoNotMatch, setPasswordDoNotMatch] = useState(false)
+    const [showPassword, setShowPassword] = useState(false)
+    const [showRepeatPassword, setShowRepeatPassword] = useState(false)
+    const [strength, setStrength] = useState(0)
+    const {errors, handleSubmit, register, watch, clearError} = useForm()
+
+    const handlePasswordChange = (e) => {
+        setPasswordDoNotMatch(false)
+        setAnchorEl(e.currentTarget)
+        let howStrong = validatePassword(e.target.value)
+        setStrength(howStrong)
+        howStrong >= 4 ? clearError("password") : setError("password")
+    }
+
+    const handleRepeatPasswordChange = (e) => {
+        setPasswordDoNotMatch(false)
+        let howStrong = validatePassword(e.target.value)
+        setStrength(howStrong)
+        howStrong >= 4 ? clearError("repeatPassword") : setError("repeatPassword")
+    }
+
+    const onSubmit = (data) => {
+        if(password === repeatPassword){
+            console.log(data)
+        } else {
+            setPasswordDoNotMatch(true)
         }
+        requestPasswordChange()
     }
 
-    handlePasswordChange = (e) => {
-        this.setState({
-            password: e.target.value,
-            passwordError: validatePassword(e.target.value) >= 4 ? false : true,
-            anchorEl: e.currentTarget,
-            strength: validatePassword(e.target.value)
-        })
+    const requestPasswordChange = (params) => {
+
     }
 
-    handleRepeatPasswordChange = (e) => {
-        this.setState({
-            repeatPassword: e.target.value,
-            repeatPasswordError: validatePassword(e.target.value) >= 4 && e.target.value === this.state.password ? false : true,
-            anchorEl: e.currentTarget,
-            strength: validatePassword(e.target.value)
-        })
-    }
-
-    handleSubmit = () => {
-        const params = {
-            password: this.state.password,
-            token: this.props.token,
-        }
-        this.requestPasswordChange(params)
-    }
-
-    requestPasswordChange = (params) => {
-        axios.post(RECOVER3_URL, params)
-            .then(() => {
-                this.props.onSuccess()
-            })
-            .catch((err) => {
-                this.props.dispatch(alertError(err.response.data.error.msg))
-            })
-    }
-
-    handleKeyPress = (event) => {
-        if (event.key === 'Enter') {
-            this.handleSubmit()
-        }
-    }
-
-    showLoader = () => {
-        this.setState({loading: true})
-    }
-
-    hideLoader = () => {
-        this.setState({loading: false})
-    }
-
-    disabledButton = () => {
-        if(this.state.password && this.state.repeatPassword && this.props.token){
-            if(!this.state.passwordError && !this.state.repeatPasswordError){
-                return false
-            }
-            return true
-        }
-        return true
-    }
-
-    render(){
-        return(
+    return(
+        <form onSubmit={handleSubmit(onSubmit)}>
             <Box className="reset-password-form" display="flex" flexDirection="column" justifyContent="center" alignItems="center">
                 <h3>Reset Password From</h3>
                 <Popper
                     className="password-strength-box"
                     placement="left"
                     transition
-                    open={this.state.anchorEl ? true : false}
-                    anchorEl={this.state.anchorEl}>
-                    <PasswordStrength onClose={() => this.setState({anchorEl: undefined})} strength={this.state.strength} />
+                    open={anchorEl ? true : false}
+                    anchorEl={anchorEl}>
+                    <PasswordStrength onClose={() => setAnchorEl(undefined)} strength={strength} />
                 </Popper>
                 <Box className="input-box" display="flex" flexDirection="column" justifyContent="start" alignItems="start">
                     <label className="custom-label">New Password</label>
                     <FormControl className="custom-input" variant="outlined">
                         <OutlinedInput
-                            type={this.state.showPassword ? 'text' : 'password'}
-                            value={this.state.password}
-                            error={this.state.passwordError}
-                            onBlur={() => this.setState({anchorEl: undefined})}
-                            onChange={this.handlePasswordChange}
-                            onKeyDown={this.handleKeyPress}
+                            type={showPassword ? 'text' : 'password'}
+                            error={errors.password ? true : false}
+                            onBlur={() => setAnchorEl(undefined)}
+                            inputRef={register(required)}
+                            onChange={handlePasswordChange}
+                            name="password"
                             endAdornment={
                             <InputAdornment position="end">
-                                <IconButton onClick={() => this.setState({showPassword: !this.state.showPassword})}>
-                                    {this.state.showPassword ? <Visibility /> : <VisibilityOff />}
+                                <IconButton onClick={() => setShowPassword(!showPassword)}>
+                                    {howPassword ? <Visibility /> : <VisibilityOff />}
                                 </IconButton>
                             </InputAdornment>
                             }/>
@@ -122,33 +84,31 @@ class TwoFAForm extends Component{
                     <label className="custom-label">Repeat Password</label>
                     <FormControl className="custom-input" variant="outlined">
                         <OutlinedInput
-                            type={this.state.showRepeatPassword ? 'text' : 'password'}
-                            value={this.state.repeatPassword}
-                            error={this.state.repeatPasswordError}
-                            onBlur={() => this.setState({anchorEl: undefined})}
-                            onChange={this.handleRepeatPasswordChange}
-                            onKeyDown={this.handleKeyPress}
+                            type={showRepeatPassword ? 'text' : 'password'}
+                            error={errors.repeatPassword ? true : false}
+                            onChange={handleRepeatPasswordChange}
+                            inputRef={register(required)}
+                            name="repeatPassword"
                             endAdornment={
                             <InputAdornment position="end">
-                                <IconButton onClick={() => this.setState({showRepeatPassword: !this.state.showRepeatPassword})}>
-                                    {this.state.showRepeatPassword ? <Visibility /> : <VisibilityOff />}
+                                <IconButton onClick={() => setShowRepeatPassword(!showRepeatPassword)}>
+                                    {showRepeatPassword ? <Visibility /> : <VisibilityOff />}
                                 </IconButton>
                             </InputAdornment>
                             }/>
                     </FormControl>
-                    {this.state.repeatPasswordError && <span className="error-text">Passwords do not match</span>}
+                    {passwordDoNotMatch && <span className="error-text">Passwords do not match</span>}
                 </Box>
 
                 <Box className="input-box">
                     <CustomButton
-                        className={"full " + (this.disabledButton() ? "disabled-btn" : "submit-btn")}
-                        disabled={this.disabledButton()}
-                        loading={this.state.loading}
-                        onClick= {this.handleSubmit}>Sign in</CustomButton>
+                        className={"full submit-btn"}
+                        loading={loading}
+                        onClick= {handleSubmit}>Sign in</CustomButton>
                 </Box>
             </Box>
-        )
-    }
+        </form>
+    )
 }
 
 export default TwoFAForm;
