df28eae1497ac7a8dadddf9fa507432585107b7e
Murad Malik <muradmalik7@gmail.com>
2020-05-15 15:41:21 +0500

    ZIG-226: crypto composition translations added

--- src/components/Balance/CryptoComposition/CryptoComposition.js
+++ src/components/Balance/CryptoComposition/CryptoComposition.js
@@ -2,11 +2,12 @@ import React from 'react';
 import './CryptoComposition.sass';
 import { Box, Typography } from '@material-ui/core';
 import Doughnut from '../../Graphs/Doughnut';
+import {FormattedMessage} from 'react-intl';
 
 const CryptoComposition = (props) => {
     return (
         <Box display="flex" flexDirection="column" justifyContent="flex-start" alignItems="flex-start" className="cryptoComposition">
-            <Typography variant="h3" className="boxTitle">Crypto Composition</Typography>
+            <Typography variant="h3" className="boxTitle"><FormattedMessage id="dashboard.balance.cryptocompo" /></Typography>
             <Doughnut />
         </Box>
     )
