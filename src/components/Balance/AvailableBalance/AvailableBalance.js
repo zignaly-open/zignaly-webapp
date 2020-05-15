3d49f82dfbb836be67e3225d003cf228de5b17ed
Murad Malik <muradmalik7@gmail.com>
2020-05-15 15:47:36 +0500

    ZIG-226: translations added to available balance component

--- src/components/Balance/AvailableBalance/AvailableBalance.js
+++ src/components/Balance/AvailableBalance/AvailableBalance.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import './AvailableBalance.sass';
 import { Box, Typography } from '@material-ui/core';
+import {FormattedMessage} from 'react-intl';
 
 const AvailableBalance = (props) => {
     return (
         <Box display="flex" flexDirection="row" justifyContent="space-between" alignItems="center" className="availableBalance">
             <Box className="dataBox" display="flex" flexDirection="column" justifyContent="flex-start" alignItems="flex-start">
                 <Box mb={1} display="flex" flexDirection="row" justifyContent="space-between" alignItems="center">
-                    <Typography variant="h4">Available </Typography>
+                    <Typography variant="h4">
+                        <FormattedMessage id="dashboard.balance.available" />
+                    </Typography>
                     <Typography variant="subtitle2" className="smallText"> = USD 3450.6</Typography>
                 </Box>
                 <Typography variant="h5">BTC 1.5646</Typography>
@@ -15,7 +18,9 @@ const AvailableBalance = (props) => {
             <span className="operator">+</span>
             <Box className="dataBox" display="flex" flexDirection="column" justifyContent="flex-start" alignItems="flex-start">
                 <Box mb={1} display="flex" flexDirection="row" justifyContent="space-between" alignItems="center">
-                    <Typography variant="h4">Invested </Typography>
+                    <Typography variant="h4">
+                        <FormattedMessage id="dashboard.balance.invested" />
+                    </Typography>
                     <Typography variant="subtitle2" className="smallText"> = USD 3450.6</Typography>
                 </Box>
                 <Typography variant="h5">BTC 1.5646</Typography>
@@ -23,7 +28,9 @@ const AvailableBalance = (props) => {
             <span className="operator">+</span>
             <Box className="dataBox" display="flex" flexDirection="column" justifyContent="flex-start" alignItems="flex-start">
                 <Box mb={1} display="flex" flexDirection="row" justifyContent="space-between" alignItems="center">
-                    <Typography variant="h4">Profit </Typography>
+                    <Typography variant="h4">
+                        <FormattedMessage id="dashboard.balance.profit" />
+                    </Typography>
                     <Typography variant="subtitle2" className="smallText"> = USD 3450.6</Typography>
                 </Box>
                 <Typography variant="h5">BTC 1.5646</Typography>
@@ -31,7 +38,9 @@ const AvailableBalance = (props) => {
             <span className="operator">=</span>
             <Box className="dataBox" display="flex" flexDirection="column" justifyContent="flex-start" alignItems="flex-start">
                 <Box mb={1} display="flex" flexDirection="row" justifyContent="space-between" alignItems="center">
-                    <Typography variant="h4">Total </Typography>
+                    <Typography variant="h4">
+                        <FormattedMessage id="dashboard.balance.total" />
+                    </Typography>
                     <Typography variant="subtitle2" className="smallText"> = USD 3450.6</Typography>
                 </Box>
                 <Typography variant="h5">BTC 1.5646</Typography>
