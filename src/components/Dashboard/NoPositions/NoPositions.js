8b225df97e9a635747537c2af22f451e655eead0
Christophe Wagner <chris.wagn22@gmail.com>
2020-05-16 20:41:16 +0700

    ZIG-420: Fix conflicts.

--- src/components/Dashboard/NoPositions/NoPositions.js
+++ src/components/Dashboard/NoPositions/NoPositions.js
@@ -1,22 +1,31 @@
-import React from 'react';
-import './NoPositions.sass';
-import { Box, Typography } from '@material-ui/core';
-import {FormattedMessage} from 'react-intl';
-import CustomButton from '../../CustomButton';
-import {navigate} from '@reach/router';
+import React from "react"
+import "./NoPositions.scss"
+import { Box, Typography } from "@material-ui/core"
+import { FormattedMessage } from "react-intl"
+import CustomButton from "../../CustomButton"
+import { navigate } from "@reach/router"
 
 const NoPositions = () => {
+  const redirect = () => {
+    navigate("/copyTraders")
+  }
 
-    const redirect = () => {
-        navigate("/copyTraders")
-    }
-
-    return (
-        <Box display="flex" flexDirection="column" justifyContent="center" alignItems="center" className="noPositions">
-            <Typography variant="h3"><FormattedMessage id="dashboard.positions.nopositions" /></Typography>
-            <CustomButton onClick={redirect} className="submit-btn"><FormattedMessage id="dashboard.browsetraders" /></CustomButton>
-        </Box>
-    )
+  return (
+    <Box
+      display="flex"
+      flexDirection="column"
+      justifyContent="center"
+      alignItems="center"
+      className="noPositions"
+    >
+      <Typography variant="h3">
+        <FormattedMessage id="dashboard.positions.nopositions" />
+      </Typography>
+      <CustomButton onClick={redirect} className="submit-btn">
+        <FormattedMessage id="dashboard.browsetraders" />
+      </CustomButton>
+    </Box>
+  )
 }
 
-export default NoPositions;
+export default NoPositions
