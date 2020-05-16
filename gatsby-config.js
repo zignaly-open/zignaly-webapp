cacc19a073da4a9c6cd9a098010010b6a076ad4c
Pablo Cerda <pablo@citla.com>
2020-05-16 23:29:47 +0200

    ZIG-281: Enable ESLint on Gatsby build.

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,7 +9,7 @@ module.exports = {
   },
   plugins: [
     {
-      resolve: "gatsby-plugin-sass",
+      resolve: `gatsby-plugin-sass`,
       options: {
         data: `@import "${__dirname}/src/styles/index";`,
       },
@@ -53,6 +53,18 @@ module.exports = {
         },
         // Exclude global styles from the plugin using a RegExp:
         exclude: `\/global\/`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-eslint`,
+      options: {
+        test: /\.js$|\.jsx$/,
+        exclude: /(node_modules|.cache|public)/,
+        stages: ["develop"],
+        options: {
+          emitWarning: true,
+          failOnError: false,
+        },
       },
     },
   ],
