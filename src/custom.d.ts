8c5e2744787e9174829f238cb566dc5c670f2b12
Pablo Cerda <pablo@citla.com>
2020-05-17 22:16:18 +0200

    ZIG-281: Fix typescript not found svg, ttf, yaml modules.

---
+++ src/custom.d.ts
@@ -0,0 +1,14 @@
+declare module "*.svg" {
+  const content: string;
+  export default content;
+}
+
+declare module "*.ttf" {
+  const content: string;
+  export default content;
+}
+
+declare module "*.yaml" {
+  const content: string;
+  export default content;
+}
