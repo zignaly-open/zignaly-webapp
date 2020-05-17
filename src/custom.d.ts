53a771de08d19eb317127454403f9ccb37213d01
Pablo Cerda <pablo@citla.com>
2020-05-17 22:18:32 +0200

    ZIG-281: Fix typescript not found png modules.

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -3,12 +3,27 @@ declare module "*.svg" {
   export default content;
 }
 
+declare module "*.png" {
+  const content: string;
+  export default content;
+}
+
+declare module "*.jpg" {
+  const content: string;
+  export default content;
+}
+
 declare module "*.ttf" {
   const content: string;
   export default content;
 }
 
 declare module "*.yaml" {
+  const content: string;
+  export default content;
+}
+
+declare module "*.yml" {
   const content: string;
   export default content;
 }
