8490bb65080cac0db235fdd70be22d11dcb59a99
Pablo Cerda <pablo@citla.com>
2020-05-20 21:10:59 +0200

    ZIG-281: Remove gatsby sharp, slow builds.

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,8 +29,6 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
-    "gatsby-transformer-sharp",
-    "gatsby-plugin-sharp",
     {
       resolve: "gatsby-plugin-manifest",
       options: {
