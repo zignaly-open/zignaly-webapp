0496d592148e4866790d692431326337564bb7f2
Pablo Cerda <pablo@citla.com>
2020-05-21 17:48:53 +0200

    ZIG-281: Remove Gatsby manifest plugin, restore for launch.

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,19 +30,6 @@ module.exports = {
       },
     },
     {
-      resolve: "gatsby-plugin-manifest",
-      options: {
-        name: "gatsby-starter-default",
-        short_name: "Zignaly",
-        start_url: "/",
-        background_color: "#663399",
-        theme_color: "#663399",
-        display: "minimal-ui",
-        // This path is relative to the root of the site.
-        icon: "src/images/gatsby-icon.png",
-      },
-    },
-    {
       resolve: "gatsby-plugin-purgecss",
       options: {
         printRejected: true,
