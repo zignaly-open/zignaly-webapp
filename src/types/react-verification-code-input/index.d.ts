6d1fa90787c1417c43947280c8a48fbff0f460ed
Pablo Cerda <pablo@citla.com>
2020-05-25 18:55:18 +0200

    ZIG-281: Complete the ReactCodeInput typedef definition.

--- src/types/react-verification-code-input/index.d.ts
+++ src/types/react-verification-code-input/index.d.ts
@@ -1,21 +1,22 @@
-import * as React from "react";
+import React from "react";
 
 // Attempts to override wrong typdef from contrib component following technique:
 // https://www.detroitlabs.com/blog/2018/02/28/adding-custom-type-definitions-to-a-third-party-library/
 // This is not working and the default typedef is taking precedence.
 
-declare module "react-verification-code-input" {
-  interface ReactCodeInputProps {
-    type: string;
-    onChange: (val: string) => void;
-    onComplete: (val: string) => void;
-    fields: number;
-    loading: boolean;
-    title: string;
-    fieldWidth: number;
-    fieldHeight: number;
-    autoFocus: boolean;
-    className: string;
-  }
-  export class ReactCodeInput extends React.Component<ReactCodeInputProps, any> {}
+interface ReactCodeInputProps {
+  type: string;
+  onChange: (val: string) => void;
+  onComplete: (val: string) => void;
+  fields: number;
+  loading: boolean;
+  title: string;
+  fieldWidth: number;
+  fieldHeight: number;
+  autoFocus: boolean;
+  className: string;
 }
+
+declare class ReactCodeInput extends React.Component<ReactCodeInputProps, any> {}
+
+export default ReactCodeInput;
