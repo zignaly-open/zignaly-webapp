18decdbe1a750c136105dbf9c6a23c3cd630f3f9
Pablo Cerda <pablo@citla.com>
2020-05-25 13:07:37 +0200

    ZIG-281: Fix LocalizedLink type error.

--- src/components/LocalizedLink/LocalizedLink.jsx
+++ src/components/LocalizedLink/LocalizedLink.jsx
@@ -3,10 +3,25 @@ import { Link } from "gatsby";
 import { getLocalizedPath } from "../../i18n";
 import { PageContext } from "../../pageContext";
 
-const LocalizedLink = ({ to, ...props }) => (
-  <PageContext.Consumer>
-    {({ locale }) => <Link {...props} to={getLocalizedPath(to, locale)} />}
-  </PageContext.Consumer>
-);
+/**
+ * @typedef {Object} LocalizedLinkProps
+ * @property {string} to
+ */
+
+/**
+ * Localized link component.
+ *
+ * @param {LocalizedLinkProps} props Link properties.
+ *
+ * @returns {JSX.Element} Localized link element.
+ */
+const LocalizedLink = (props) => {
+  const { to } = props;
+  return (
+    <PageContext.Consumer>
+      {({ locale }) => <Link {...props} to={getLocalizedPath(to, locale)} />}
+    </PageContext.Consumer>
+  );
+};
 
 export default LocalizedLink;
