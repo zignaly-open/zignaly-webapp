2b18467168717d88c6b21844a1cf3c170be28b39
Pablo Cerda <pablo@citla.com>
2020-05-25 18:50:04 +0200

    ZIG-281: Comment typdef override technique tried for ReactCodeInput.

--- src/types/react-verification-code-input/index.d.ts
+++ src/types/react-verification-code-input/index.d.ts
@@ -1,5 +1,9 @@
 import * as React from "react";
 
+// Attempts to override wrong typdef from contrib component following technique:
+// https://www.detroitlabs.com/blog/2018/02/28/adding-custom-type-definitions-to-a-third-party-library/
+// This is not working and the default typedef is taking precedence.
+
 declare module "react-verification-code-input" {
   interface ReactCodeInputProps {
     type: string;
