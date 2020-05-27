3876f2459fce4052ceba1f19411bbee6ef583888
Pablo Cerda <pablo@citla.com>
2020-05-27 10:13:21 +0200

    ZIG-222: Add PositionTable headers for all display fields.

--- src/components/Dashboard/PositionsTable/PositionsTableHead/PositionsTableHead.js
+++ src/components/Dashboard/PositionsTable/PositionsTableHead/PositionsTableHead.js
@@ -7,37 +7,76 @@ const PositionsTableHead = () => {
     <TableHead className="tableHead">
       <TableRow className="row">
         <TableCell align="left" className="cell">
-          Time
+          {" "}
         </TableCell>
         <TableCell align="left" className="cell">
-          Type
+          Open Date
+        </TableCell>
+        <TableCell align="left" className="cell">
+          Provider
+        </TableCell>
+        <TableCell align="left" className="cell">
+          Signal ID
         </TableCell>
         <TableCell align="left" className="cell">
           Pair
         </TableCell>
         <TableCell align="left" className="cell">
-          Trader
-        </TableCell>
-        <TableCell align="left" className="cell">
-          Invested
-        </TableCell>
-        <TableCell align="left" className="cell">
           Entry Price
+        </TableCell>
+        <TableCell align="left" className="cell">
+          Leverage
         </TableCell>
         <TableCell align="left" className="cell">
           Current Price
         </TableCell>
         <TableCell align="left" className="cell">
-          P/L %
-        </TableCell>
-        <TableCell align="left" className="cell">
           P/L #
         </TableCell>
         <TableCell align="left" className="cell">
-          Net Profit
+          Price Difference
+        </TableCell>
+        <TableCell align="left" className="cell">
+          Type
+        </TableCell>
+        <TableCell align="left" className="cell">
+          Stop Loss Price
+        </TableCell>
+        <TableCell align="left" className="cell">
+          Initial Amount
+        </TableCell>
+        <TableCell align="left" className="cell">
+          Remaining Amount
+        </TableCell>
+        <TableCell align="left" className="cell">
+          Invested
+        </TableCell>
+        <TableCell align="left" className="cell">
+          TSL
+        </TableCell>
+        <TableCell align="left" className="cell">
+          TP
+        </TableCell>
+        <TableCell align="left" className="cell">
+          DCA
         </TableCell>
         <TableCell align="left" className="cell">
           Risk
+        </TableCell>
+        <TableCell align="left" className="cell">
+          Age
+        </TableCell>
+        <TableCell align="left" className="cell">
+          Open Trigger
+        </TableCell>
+        <TableCell align="left" className="cell">
+          Edit
+        </TableCell>
+        <TableCell align="left" className="cell">
+          Exit Position
+        </TableCell>
+        <TableCell align="left" className="cell">
+          Cancel Position
         </TableCell>
       </TableRow>
     </TableHead>
