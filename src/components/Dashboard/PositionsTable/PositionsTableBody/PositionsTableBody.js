e9108d7616930a40c074f25c7ce7653ac94e9d7a
Pablo Cerda <pablo@citla.com>
2020-05-28 20:36:11 +0200

    ZIG-552: Wrap actions in buttons and handle confirm dialog display.

--- src/components/Dashboard/PositionsTable/PositionsTableBody/PositionsTableBody.js
+++ src/components/Dashboard/PositionsTable/PositionsTableBody/PositionsTableBody.js
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "gatsby";
 import "./PositionsTableBody.scss";
 import { TableBody, TableRow, TableCell } from "@material-ui/core";
 import { Edit2, Eye, File, LogOut, TrendingUp, XCircle } from "react-feather";
 import { colors } from "../../../../services/theme";
 import { formatNumber } from "../../../../utils/formatters";
+import { ConfirmDialog } from "../../../Dialogs";
 
 /**
  * @typedef {import("../../../../services/tradeApiClient.types").PositionEntity} PositionEntity
@@ -26,6 +27,21 @@ import { formatNumber } from "../../../../utils/formatters";
  */
 const PositionsTableBody = (props) => {
   const { positions, type } = props;
+  const [confirmVisible, setConfirmVisible] = useState(false);
+
+  /**
+   * Handle action element click event.
+   *
+   * @param {React.MouseEvent<HTMLButtonElement>} event Action element click.
+   * @returns {Void} None.
+   */
+  const confirmAction = () => {
+    // const targetElement = event.currentTarget;
+    // const positionId = targetElement.getAttribute("data-position-id");
+    // const action = targetElement.getAttribute("data-action");
+
+    setConfirmVisible(true);
+  };
 
   /**
    * Compose provider icon column content.
@@ -66,181 +82,190 @@ const PositionsTableBody = (props) => {
   };
 
   return (
-    <TableBody className="tableBody">
-      {positions.map((position) => (
-        <TableRow className="row" key={position.positionId}>
-          <TableCell align="left" className="cell">
-            {position.paperTrading && <File color={colors.darkGrey} />}
-          </TableCell>
-          <TableCell align="left" className="cell">
-            {position.openDateReadable}
-          </TableCell>
-          {type === "closed" && (
+    <>
+      <ConfirmDialog confirmVisible={confirmVisible} setConfirmVisible={setConfirmVisible} />
+      <TableBody className="tableBody">
+        {positions.map((position) => (
+          <TableRow className="row" key={position.positionId}>
             <TableCell align="left" className="cell">
-              {position.closeDateReadable}
+              {position.paperTrading && <File color={colors.darkGrey} />}
             </TableCell>
-          )}
-          <TableCell align="left" className="cell">
-            {composeProviderIcon(position)}
-          </TableCell>
-          <TableCell align="left" className="cell">
-            {position.providerName}
-          </TableCell>
-          {["closed", "log"].includes(type) && (
             <TableCell align="left" className="cell">
-              {position.statusDesc}
+              {position.openDateReadable}
             </TableCell>
-          )}
-          <TableCell align="left" className="cell">
-            {position.signalId}
-          </TableCell>
-          <TableCell align="left" className="cell">
-            {position.pair}
-          </TableCell>
-          <TableCell align="left" className="cell">
-            {position.buyPrice}
-          </TableCell>
-          {type === "open" && (
-            <TableCell align="left" className="cell">
-              {position.leverage}
-            </TableCell>
-          )}
-          {["closed", "open"].includes(type) && (
-            <>
+            {type === "closed" && (
               <TableCell align="left" className="cell">
-                {position.sellPrice}
+                {position.closeDateReadable}
               </TableCell>
-              <TableCell align="left" className="cell">
-                {position.status === 1 ? (
-                  <span>Still entering...</span>
-                ) : (
-                  <span className={position.profitStyle}>{position.profit}</span>
-                )}
-              </TableCell>
-              <TableCell align="left" className="cell">
-                {position.status === 1 ? (
-                  <span>Still entering...</span>
-                ) : (
-                  <span className={position.profitStyle}>{position.profitPercentage}</span>
-                )}
-              </TableCell>
-            </>
-          )}
-          <TableCell align="left" className="cell">
-            {position.side}
-          </TableCell>
-          {["closed", "open"].includes(type) && (
-            <TableCell align="left" className="cell">
-              <span className={position.stopLossStyle}>{position.stopLossPrice}</span>
-            </TableCell>
-          )}
-          <TableCell align="left" className="cell">
-            {position.amount}
-          </TableCell>
-          {["open", "log"].includes(type) && (
-            <TableCell align="left" className="cell">
-              {position.remainAmount}
-            </TableCell>
-          )}
-          <TableCell align="left" className="cell">
-            {position.positionSizeQuote}
-          </TableCell>
-          {["closed", "open"].includes(type) && (
-            <>
-              <TableCell align="left" className="cell">
-                {composeTrailingStopIcon(position)}
-              </TableCell>
-              <TableCell align="left" className="cell">
-                {position.takeProfitTargetsCountFail > 0 && (
-                  <span className="targetRed" title="Take profits failed.">
-                    {position.takeProfitTargetsCountFail}
-                  </span>
-                )}
-                {position.takeProfitTargetsCountSuccess > 0 && (
-                  <span className="targetGreen" title="Take profits successfully completed.">
-                    {position.takeProfitTargetsCountSuccess}
-                  </span>
-                )}
-                {position.takeProfitTargetsCountPending > 0 && (
-                  <span className="targetGray" title="Pending take profits.">
-                    {position.takeProfitTargetsCountPending}
-                  </span>
-                )}
-              </TableCell>
-              <TableCell align="left" className="cell">
-                {position.reBuyTargetsCountFail > 0 && (
-                  <span className="targetRed" title="DCAs failed.">
-                    {position.reBuyTargetsCountFail}
-                  </span>
-                )}
-                {position.reBuyTargetsCountSuccess > 0 && (
-                  <span className="targetGreen" title="DCAs successfully completed.">
-                    {position.reBuyTargetsCountSuccess}
-                  </span>
-                )}
-                {position.reBuyTargetsCountPending > 0 && (
-                  <span className="targetGray" title="Pending DCAs">
-                    {position.reBuyTargetsCountPending}
-                  </span>
-                )}
-              </TableCell>
-              <TableCell align="left" className="cell">
-                <span className={position.riskStyle}>{position.risk.toFixed(2)} %</span>
-              </TableCell>
-            </>
-          )}
-          {type === "open" && (
-            <TableCell align="left" className="cell">
-              {position.age}
-            </TableCell>
-          )}
-          {["closed", "open"].includes(type) && (
-            <TableCell align="left" className="cell">
-              {position.openTrigger}
-            </TableCell>
-          )}
-          {type === "closed" && (
-            <>
-              <TableCell align="left" className="cell">
-                {formatNumber(position.fees)}
-              </TableCell>
-              <TableCell align="left" className="cell">
-                {formatNumber(position.netProfitPercentage, 2)}
-              </TableCell>
-              <TableCell align="left" className="cell">
-                {formatNumber(position.netProfit)}
-              </TableCell>
-            </>
-          )}
-          <TableCell align="right" className="cell actions">
-            {["closed", "log"].includes(type) && (
-              <span title="view">
-                <Eye color={colors.purpleLight} />
-              </span>
             )}
+            <TableCell align="left" className="cell">
+              {composeProviderIcon(position)}
+            </TableCell>
+            <TableCell align="left" className="cell">
+              {position.providerName}
+            </TableCell>
+            {["closed", "log"].includes(type) && (
+              <TableCell align="left" className="cell">
+                {position.statusDesc}
+              </TableCell>
+            )}
+            <TableCell align="left" className="cell">
+              {position.signalId}
+            </TableCell>
+            <TableCell align="left" className="cell">
+              {position.pair}
+            </TableCell>
+            <TableCell align="left" className="cell">
+              {position.buyPrice}
+            </TableCell>
             {type === "open" && (
+              <TableCell align="left" className="cell">
+                {position.leverage}
+              </TableCell>
+            )}
+            {["closed", "open"].includes(type) && (
               <>
-                {position.isCopyTrading ? (
-                  <span title="view">
-                    <Eye color={colors.purpleLight} />
-                  </span>
-                ) : (
-                  <span title="edit">
-                    <Edit2 color={colors.purpleLight} />
-                  </span>
-                )}
-                <span title="exit">
-                  <LogOut color={colors.purpleLight} />
-                </span>
-                <span title="cancel">
-                  <XCircle color={colors.purpleLight} />
-                </span>
+                <TableCell align="left" className="cell">
+                  {position.sellPrice}
+                </TableCell>
+                <TableCell align="left" className="cell">
+                  {position.status === 1 ? (
+                    <span>Still entering...</span>
+                  ) : (
+                    <span className={position.profitStyle}>{position.profit}</span>
+                  )}
+                </TableCell>
+                <TableCell align="left" className="cell">
+                  {position.status === 1 ? (
+                    <span>Still entering...</span>
+                  ) : (
+                    <span className={position.profitStyle}>{position.profitPercentage}</span>
+                  )}
+                </TableCell>
               </>
             )}
-          </TableCell>
-        </TableRow>
-      ))}
-    </TableBody>
+            <TableCell align="left" className="cell">
+              {position.side}
+            </TableCell>
+            {["closed", "open"].includes(type) && (
+              <TableCell align="left" className="cell">
+                <span className={position.stopLossStyle}>{position.stopLossPrice}</span>
+              </TableCell>
+            )}
+            <TableCell align="left" className="cell">
+              {position.amount}
+            </TableCell>
+            {["open", "log"].includes(type) && (
+              <TableCell align="left" className="cell">
+                {position.remainAmount}
+              </TableCell>
+            )}
+            <TableCell align="left" className="cell">
+              {position.positionSizeQuote}
+            </TableCell>
+            {["closed", "open"].includes(type) && (
+              <>
+                <TableCell align="left" className="cell">
+                  {composeTrailingStopIcon(position)}
+                </TableCell>
+                <TableCell align="left" className="cell">
+                  {position.takeProfitTargetsCountFail > 0 && (
+                    <span className="targetRed" title="Take profits failed.">
+                      {position.takeProfitTargetsCountFail}
+                    </span>
+                  )}
+                  {position.takeProfitTargetsCountSuccess > 0 && (
+                    <span className="targetGreen" title="Take profits successfully completed.">
+                      {position.takeProfitTargetsCountSuccess}
+                    </span>
+                  )}
+                  {position.takeProfitTargetsCountPending > 0 && (
+                    <span className="targetGray" title="Pending take profits.">
+                      {position.takeProfitTargetsCountPending}
+                    </span>
+                  )}
+                </TableCell>
+                <TableCell align="left" className="cell">
+                  {position.reBuyTargetsCountFail > 0 && (
+                    <span className="targetRed" title="DCAs failed.">
+                      {position.reBuyTargetsCountFail}
+                    </span>
+                  )}
+                  {position.reBuyTargetsCountSuccess > 0 && (
+                    <span className="targetGreen" title="DCAs successfully completed.">
+                      {position.reBuyTargetsCountSuccess}
+                    </span>
+                  )}
+                  {position.reBuyTargetsCountPending > 0 && (
+                    <span className="targetGray" title="Pending DCAs">
+                      {position.reBuyTargetsCountPending}
+                    </span>
+                  )}
+                </TableCell>
+                <TableCell align="left" className="cell">
+                  <span className={position.riskStyle}>{position.risk.toFixed(2)} %</span>
+                </TableCell>
+              </>
+            )}
+            {type === "open" && (
+              <TableCell align="left" className="cell">
+                {position.age}
+              </TableCell>
+            )}
+            {["closed", "open"].includes(type) && (
+              <TableCell align="left" className="cell">
+                {position.openTrigger}
+              </TableCell>
+            )}
+            {type === "closed" && (
+              <>
+                <TableCell align="left" className="cell">
+                  {formatNumber(position.fees)}
+                </TableCell>
+                <TableCell align="left" className="cell">
+                  {formatNumber(position.netProfitPercentage, 2)}
+                </TableCell>
+                <TableCell align="left" className="cell">
+                  {formatNumber(position.netProfit)}
+                </TableCell>
+              </>
+            )}
+            <TableCell align="right" className="cell actions">
+              {["closed", "log"].includes(type) && (
+                <button title="view" type="button">
+                  <Eye color={colors.purpleLight} />
+                </button>
+              )}
+              {type === "open" && (
+                <>
+                  {position.isCopyTrading ? (
+                    <button title="view" type="button">
+                      <Eye color={colors.purpleLight} />
+                    </button>
+                  ) : (
+                    <button title="edit" type="button">
+                      <Edit2 color={colors.purpleLight} />
+                    </button>
+                  )}
+                  <button title="exit" type="button">
+                    <LogOut color={colors.purpleLight} />
+                  </button>
+                  <button
+                    data-action={"cancel"}
+                    data-position-id={position.positionId}
+                    onClick={confirmAction}
+                    title="cancel"
+                    type="button"
+                  >
+                    <XCircle color={colors.purpleLight} />
+                  </button>
+                </>
+              )}
+            </TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </>
   );
 };
 
