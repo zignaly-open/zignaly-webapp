d9aa3f725df256eb1d4fa9eb79294b57a3c5e298
Pablo Cerda <pablo@citla.com>
2020-05-29 14:48:42 +0200

    ZIG-552: Fix ReferenceError: locale is not defined.

--- src/components/LanguageSwitcher/LanguageSwitcher.js
+++ src/components/LanguageSwitcher/LanguageSwitcher.js
@@ -3,6 +3,7 @@ import cs from "../../images/cs.png";
 import en from "../../images/en.png";
 import { languages } from "../../i18n";
 import "./languageSwitcher.scss";
+import useStoreSettingsSelector from "../../hooks/useStoreSettingsSelector";
 
 /**
  * @type {Object.<string, string>} flags
@@ -12,18 +13,22 @@ const flags = {
   en,
 };
 
-const LanguageSwitcher = () => (
-  <div className="languageSwitcher">
-    {languages.map((lang) =>
-      lang.locale === locale ? (
-        <img alt={lang.label} key={lang.locale} src={flags[lang.locale]} />
-      ) : (
-        <button type="button">
-          <img alt={lang.label} src={flags[lang.locale]} />
-        </button>
-      ),
-    )}
-  </div>
-);
+const LanguageSwitcher = () => {
+  const storeSettings = useStoreSettingsSelector();
+
+  return (
+    <div className="languageSwitcher">
+      {languages.map((lang) =>
+        lang.locale === storeSettings.languageCode ? (
+          <img alt={lang.label} key={lang.locale} src={flags[lang.locale]} />
+        ) : (
+          <button type="button">
+            <img alt={lang.label} src={flags[lang.locale]} />
+          </button>
+        ),
+      )}
+    </div>
+  );
+};
 
 export default LanguageSwitcher;
