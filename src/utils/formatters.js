ecf0ac7b42e35c08223958ab07678d521f82b871
Pablo Cerda <pablo@citla.com>
2020-05-29 20:47:45 +0200

    ZIG-222: Smart format price dynamic precision function.

--- src/utils/formatters.js
+++ src/utils/formatters.js
@@ -13,3 +13,24 @@ export const formatNumber = (value, precision = 8) => {
 
   return value.toFixed(precision);
 };
+
+/**
+ * Format price for display.
+ *
+ * Numbers greater than 1 show 2 digits precision, floats show 8 digits precision.
+ *
+ * @param {number} price Price to format.
+ *
+ * @returns {string} Formatter price for display.
+ */
+export const formatPrice = (price) => {
+  if (isNaN(price)) {
+    return "-";
+  }
+
+  if (price > 1) {
+    return price.toFixed(2);
+  }
+
+  return price.toFixed(8);
+};
