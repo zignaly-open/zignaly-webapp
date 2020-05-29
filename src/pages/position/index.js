88c6ac8e9411ff1f1dae8a4b133d34f24979557c
Pablo Cerda <pablo@citla.com>
2020-05-29 15:31:11 +0200

    ZIG-552: Define PositionPageProps and display positionId.

--- src/pages/position/index.js
+++ src/pages/position/index.js
@@ -6,18 +6,38 @@ import withDashboardLayout from "../../layouts/dashboardLayout";
 import withPageContext from "../../pageContext";
 import { Helmet } from "react-helmet";
 
-const Position = (props) => {
-  console.log(props);
+/**
+ * @typedef {Object} PositionPageProps
+ * @property {string} positionId The position ID dynamic route path parameter.
+ */
+
+/**
+ * Position detail page component.
+ *
+ * @param {PositionPageProps} props Component properties.
+ * @returns {JSX.Element} Position page element.
+ */
+const PositionPage = (props) => {
+  const { positionId } = props;
+
   return (
     <>
       <Helmet>
         <title>Position Detail</title>
       </Helmet>
-      <Box className="positionsPage" display="flex" flexDirection="row" justifyContent="center">
+      <Box className="positionPage" display="flex" flexDirection="column" justifyContent="center">
         <h1>I will be the position detail page.</h1>
+        <Box
+          className="positionDetail"
+          display="flex"
+          flexDirection="column"
+          justifyContent="center"
+        >
+          <h2>Viewing position: {positionId}</h2>
+        </Box>
       </Box>
     </>
   );
 };
 
-export default compose(withPageContext, withAppLayout, withDashboardLayout)(Position);
+export default compose(withPageContext, withAppLayout, withDashboardLayout)(PositionPage);
