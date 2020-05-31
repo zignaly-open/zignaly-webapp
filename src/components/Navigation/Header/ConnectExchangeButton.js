46622895d1f5d9bb606a9e987e7cf0661c899448
Murad Malik <muradmalik7@gmail.com>
2020-05-31 16:55:33 +0500

    ZIG-217: connect button is now translated

--- src/components/Navigation/Header/ConnectExchangeButton.js
+++ src/components/Navigation/Header/ConnectExchangeButton.js
@@ -2,6 +2,7 @@ import React, { Fragment, useState } from "react";
 import CustomButton from "../../CustomButton";
 import Modal from "../../Modal";
 import ConnectExchangeView from "../../ConnectExchangeView";
+import { FormattedMessage } from "react-intl";
 
 const ConnectExchangeButton = () => {
   const [open, setOpen] = useState(false);
@@ -9,7 +10,7 @@ const ConnectExchangeButton = () => {
   return (
     <Fragment>
       <CustomButton className="headerButton" onClick={() => setOpen(true)}>
-        Connect Account
+        <FormattedMessage id="menu.connectexchange" />
       </CustomButton>
       <Modal size="large" persist={false} state={open} onClose={() => setOpen(false)}>
         <ConnectExchangeView onClose={() => setOpen(false)} />
