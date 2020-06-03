26f24747e927115a5cc8bcc76d5c5278bb487570
Murad Malik <muradmalik7@gmail.com>
2020-06-03 18:57:21 +0500

    ZIG-228: number formated to 8 decimals

--- src/components/Balance/TotalEquity/TitleBar/TitleBar.js
+++ src/components/Balance/TotalEquity/TitleBar/TitleBar.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./TitleBar.scss";
 import { Box, Typography } from "@material-ui/core";
 import { FormattedMessage } from "react-intl";
-
+import { formatFloat } from "../../../../utils/format";
 /**
  *
  * @typedef {import("../../../../services/tradeApiClient.types").UserEquityEntity} UserEquityEntity
@@ -45,9 +45,11 @@ const TitleBar = (props) => {
           justifyContent="space-between"
           mt={1}
         >
-          <Typography variant="h4">BTC {list.length ? list[0].totalBTC : "0"} </Typography>
+          <Typography variant="h4">
+            BTC {list.length ? formatFloat(list[0].totalBTC) : "0"}{" "}
+          </Typography>
           <Typography className="smallText" variant="subtitle2">
-            = USD {list.length ? list[0].totalUSD : "0"}
+            = USD {list.length ? formatFloat(list[0].totalUSD) : "0"}
           </Typography>
         </Box>
       </Box>
