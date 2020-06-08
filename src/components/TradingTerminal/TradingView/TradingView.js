c2a5ca46710ce1128c624f0fded92d6a3496e438
Pablo Cerda <pablo@citla.com>
2020-06-08 15:52:44 +0200

    ZIG-424: Extract data feed instantiation to factory hook.

--- src/components/TradingTerminal/TradingView/TradingView.js
+++ src/components/TradingTerminal/TradingView/TradingView.js
@@ -1,83 +1,15 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { widget as TradingViewWidget } from "../../../tradingView/charting_library.min";
-import useStoreSessionSelector from "../../../hooks/useStoreSessionSelector";
-import CoinRayDataFeed from "../../../services/coinRayDataFeed";
-import tradeApi from "../../../services/tradeApiClient";
-import { isEmpty, throttle } from "lodash";
 import "./TradingView.scss";
-import useStoreSettingsSelector from "../../../hooks/useStoreSettingsSelector";
 import { createWidgetOptions } from "../../../tradingView/dataFeedOptions";
+import { useCoinRayDataFeedFactory } from "../../../hooks/useCoinRayDataFeedFactory";
 
 const TradingView = () => {
-  const storeSession = useStoreSessionSelector();
-  const storeSettings = useStoreSettingsSelector();
-  const [coinRayToken, setCoinRayToken] = useState("");
-  const [marketSymbolsData, setMarketSymbolsData] = useState([]);
-
-  const getCoinrayToken = async () => {
-    const milliSecsThreshold = 20000;
-    // Limit to one request within a time threshold to avoid stampede requests to API.
-    const throttledTokenFetch = throttle(async () => {
-      try {
-        const coinRayToken = await tradeApi.coinRayTokenGet({
-          token: storeSession.tradeApi.accessToken,
-        });
-
-        return coinRayToken.jwt;
-      } catch (error) {
-        alert(`ERROR: Get coinray token error: ${error.message}`);
-      }
-
-      return "";
-    }, milliSecsThreshold);
-
-    return (await throttledTokenFetch()) || "";
-  };
-
-  const getDataFeedService = () => {
-    const symbol = "BTC/USDT";
-    // const exchangeKey = resolveCoinRayExchangeKey(exchangeData);
-    const exchangeKey = "BINA";
-    const options = {
-      exchange: "Binance",
-      exchangeKey,
-      symbol,
-      symbolsData: marketSymbolsData,
-      tradeApiToken: storeSession.tradeApi.accessToken,
-      coinRayToken: coinRayToken,
-      regenerateAccessToken: getCoinrayToken,
-    };
-
-    return new CoinRayDataFeed(options);
-  };
-
+  const dataFeed = useCoinRayDataFeedFactory();
   const createTradigViewWidget = () => {
-    const dataFeed = getDataFeedService();
     const widgetOptions = createWidgetOptions(dataFeed, "BTCUSDT");
-
     return new TradingViewWidget(widgetOptions);
   };
-
-  // Get a CoinRay token.
-  useEffect(() => {
-    getCoinrayToken().then((token) => {
-      setCoinRayToken(token);
-    });
-
-    const marketDataPayload = {
-      token: storeSession.tradeApi.accessToken,
-      exchangeInternalId: storeSettings.selectedExchange.internalId,
-    };
-
-    tradeApi
-      .exchangeConnectionMarketDataGet(marketDataPayload)
-      .then((data) => {
-        setMarketSymbolsData(data);
-      })
-      .catch((e) => {
-        alert(`ERROR: ${e.message}`);
-      });
-  }, []);
 
   const bootstrapWidget = () => {
     /**
@@ -86,7 +18,7 @@ const TradingView = () => {
      */
     let tvInstance = null;
 
-    if (coinRayToken !== "" && !isEmpty(marketSymbolsData)) {
+    if (dataFeed) {
       tvInstance = createTradigViewWidget();
     }
 
@@ -98,7 +30,7 @@ const TradingView = () => {
   };
 
   // Create Trading View widget when data feed token is ready.
-  useEffect(bootstrapWidget, [coinRayToken, marketSymbolsData]);
+  useEffect(bootstrapWidget, [dataFeed]);
 
   return <div className="tradingView" id="trading_view_chart" />;
 };
---
+++ src/hooks/useCoinRayDataFeedFactory.js
@@ -0,0 +1,81 @@
+import { useEffect, useState } from "react";
+import CoinRayDataFeed from "../services/coinRayDataFeed";
+import { throttle } from "lodash";
+import tradeApi from "../services/tradeApiClient";
+import useStoreSettingsSelector from "./useStoreSettingsSelector";
+import useStoreSessionSelector from "./useStoreSessionSelector";
+
+export const useCoinRayDataFeedFactory = () => {
+  const storeSession = useStoreSessionSelector();
+  const storeSettings = useStoreSettingsSelector();
+  const [coinRayToken, setCoinRayToken] = useState("");
+  const [marketSymbolsData, setMarketSymbolsData] = useState([]);
+  const symbol = "BTC/USDT";
+  // const exchangeKey = resolveCoinRayExchangeKey(exchangeData);
+  const exchangeKey = "BINA";
+
+  const getCoinrayToken = async () => {
+    const milliSecsThreshold = 20000;
+    // Limit to one request within a time threshold to avoid stampede requests to API.
+    const throttledTokenFetch = throttle(async () => {
+      try {
+        const data = await tradeApi.coinRayTokenGet({
+          token: storeSession.tradeApi.accessToken,
+        });
+
+        return data.jwt;
+      } catch (error) {
+        alert(`ERROR: Get coinray token error: ${error.message}`);
+      }
+
+      return "";
+    }, milliSecsThreshold);
+
+    return (await throttledTokenFetch()) || "";
+  };
+
+  const getMarketData = async () => {
+    const marketDataPayload = {
+      token: storeSession.tradeApi.accessToken,
+      exchangeInternalId: storeSettings.selectedExchange.internalId,
+    };
+
+    try {
+      const data = await tradeApi.exchangeConnectionMarketDataGet(marketDataPayload);
+
+      return data;
+    } catch (error) {
+      alert(`ERROR: ${error.message}`);
+    }
+
+    return [];
+  };
+
+  // Get a CoinRay token.
+  const resolveDataDependencies = () => {
+    const tokenPromise = getCoinrayToken();
+    const marketDataPromise = getMarketData();
+
+    Promise.all([tokenPromise, marketDataPromise]).then((data) => {
+      console.log("Dependend data: ", data);
+    });
+  };
+
+  useEffect(resolveDataDependencies, []);
+
+  const options = {
+    exchange: "Binance",
+    exchangeKey,
+    symbol,
+    symbolsData: marketSymbolsData,
+    tradeApiToken: storeSession.tradeApi.accessToken,
+    coinRayToken: coinRayToken,
+    regenerateAccessToken: getCoinrayToken,
+  };
+
+  if (coinRayToken && marketSymbolsData) {
+    return new CoinRayDataFeed(options);
+  }
+
+  return null;
+};
