af9880d8f0c94903e19e1d4bc5fd2eafd35ff1ca
Pablo Cerda <pablo@citla.com>
2020-06-09 19:46:40 +0200

    ZIG-576: Add i18n title to trading terminal page and style.

--- src/pages/tradingTerminal/index.js
+++ src/pages/tradingTerminal/index.js
@@ -1,16 +1,26 @@
 import React from "react";
+import { useIntl } from "react-intl";
 import { Box } from "@material-ui/core";
 import { Helmet } from "react-helmet";
 import { TradingView } from "../../components/TradingTerminal";
+import { FormattedMessage } from "react-intl";
+import { Typography } from "@material-ui/core";
+import "./tradingTerminal.scss";
 
 const TradingTerminal = () => {
+  const intl = useIntl();
   return (
-    <>
+    <Box className="tradingTerminalPage">
       <Helmet>
-        <title>Trading Terminal</title>
+        <title>{intl.formatMessage({ id: "menu.tradingterminal" })}</title>
       </Helmet>
+      <Box className="titleBox">
+        <Typography variant="h1">
+          <FormattedMessage id="terminal.title" />
+        </Typography>
+      </Box>
       <TradingView />
-    </>
+    </Box>
   );
 };
 
