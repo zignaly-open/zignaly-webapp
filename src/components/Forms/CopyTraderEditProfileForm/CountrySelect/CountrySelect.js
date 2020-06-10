e06b6f7a4ffad8958b1331ebaf4da50cd75ffff2
Murad Malik <muradmalik7@gmail.com>
2020-06-10 16:55:14 +0500

    ZIG-482: country select added with names and flags

---
+++ src/components/Forms/CopyTraderEditProfileForm/CountrySelect/CountrySelect.js
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import "./CountrySelect.scss";
+import { TextField } from "@material-ui/core";
+import Autocomplete from "@material-ui/lab/Autocomplete";
+import { countries } from "countries-list";
+import useStoreSettingsSelector from "../../../../hooks/useStoreSettingsSelector";
+
+/**
+ *
+ * @typedef {Object} DefaultProps
+ * @property {Function} onChange
+ */
+
+/**
+ *
+ * @param {DefaultProps} props
+ */
+
+const CountrySelect = ({ onChange }) => {
+  const storeSettings = useStoreSettingsSelector();
+  const list = Object.values(countries);
+  const [selected, setSelected] = useState([list[0]]);
+
+  /**
+   *
+   * @param {React.ChangeEvent} event
+   * @param {*} newVal
+   */
+
+  const hanldeChange = (event, newVal) => {
+    setSelected(newVal);
+    onChange(newVal);
+  };
+
+  return (
+    <Autocomplete
+      autoHighlight
+      className={"countrySelect " + (storeSettings.darkStyle ? "dark" : "light")}
+      classes={{ tag: "chip", endAdornment: "clearIcon", popupIndicator: "downBtn" }}
+      getOptionLabel={(option) => option.name}
+      multiple
+      onChange={hanldeChange}
+      options={list}
+      renderInput={(params) => <TextField {...params} variant="outlined" />}
+      renderOption={(option) => (
+        <>
+          <span className="mr">{option.emoji}</span>
+          <span className="mr">+{option.phone}</span>
+          <span className="mr">{option.name}</span>
+        </>
+      )}
+      value={selected}
+    />
+  );
+};
+
+export default CountrySelect;
--- src/pages/copyTraders/edit/index.js
+++ src/pages/copyTraders/edit/index.js
@@ -1,10 +1,16 @@
 import React from "react";
+import "./edit.scss";
 import { Box } from "@material-ui/core";
 import withProviderLayout from "../../../layouts/providerLayout";
 import { compose } from "recompose";
+import CopyTraderEditProfileForm from "../../../components/Forms/CopyTraderEditProfileForm";
 
 const CopyTradersProfile = () => {
-  return <Box>dynamic route for edit </Box>;
+  return (
+    <Box className="profileEditPage">
+      <CopyTraderEditProfileForm />
+    </Box>
+  );
 };
 
 export default compose(withProviderLayout)(CopyTradersProfile);
