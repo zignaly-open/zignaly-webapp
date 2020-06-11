747e38163e17312d27640bf9728a9cf7cc380b14
Murad Malik <muradmalik7@gmail.com>
2020-06-11 11:52:33 +0500

    ZIG-218: changed history table allocated to locked and freeBTC to totalBTCFree

--- src/store/initialState.js
+++ src/store/initialState.js
@@ -118,10 +118,10 @@ const initialState = {
       dailyBalance: [
         "date",
         "totalUSDT",
-        "freeUSDT",
+        "totalFreeUSDT",
         "lockedUSDT",
         "totalBTC",
-        "freeBTC",
+        "totalFreeBTC",
         "lockedBTC",
         "freeETH",
         "freeBNB",
