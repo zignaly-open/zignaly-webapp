532a21c6323c4245d89f4488e42ed478f472910e
Murad Malik <muradmalik7@gmail.com>
2020-06-15 17:30:49 +0500

    ZIG-403-ct-analytics-started

--- src/pages/copyTraders/providerAnalytics/index.js
+++ src/pages/copyTraders/providerAnalytics/index.js
@@ -1,10 +1,11 @@
 import React from "react";
+import "./providerAnalytics.scss";
 import { Box } from "@material-ui/core";
 import withProviderLayout from "../../../layouts/providerLayout";
 import { compose } from "recompose";
 
-const CopyTradersProfile = () => {
+const CopyTradersAnalytics = () => {
   return <Box>dynamic route for analytics </Box>;
 };
 
-export default compose(withProviderLayout)(CopyTradersProfile);
+export default compose(withProviderLayout)(CopyTradersAnalytics);
