58d7142435d5fd561c9faedf95c30b7657952563
Murad Malik <muradmalik7@gmail.com>
2020-06-18 02:37:46 +0500

    error alert component duplicated for success alert

--- src/components/Alerts/SuccessAlert/SuccessAlert.js
+++ src/components/Alerts/SuccessAlert/SuccessAlert.js
@@ -1,12 +1,12 @@
 import React from "react";
-import "./ErrorAlert.scss";
+import "./SuccessAlert.scss";
 import { Snackbar, Box, Typography, Slide } from "@material-ui/core";
 import useStoreUIAlertsSelector from "../../../hooks/useStoreUIAlertsSelector";
 import { useDispatch } from "react-redux";
 import { hideErrorAlert } from "../../../store/actions/ui";
 import { FormattedMessage } from "react-intl";
 
-const ErrorAlert = () => {
+const SuccessAlert = () => {
   const storeAlerts = useStoreUIAlertsSelector();
   const dispatch = useDispatch();
 
@@ -18,18 +18,18 @@ const ErrorAlert = () => {
       }}
       TransitionComponent={Slide}
       autoHideDuration={50000000}
-      className="errorAlert"
+      className="successAlert"
       onClose={() => dispatch(hideErrorAlert())}
       open={storeAlerts.error.open}
     >
       <Box bgcolor="grid.main" className="alertMessage">
         {storeAlerts.error.title && (
-          <Typography variant="h3" className="title red">
+          <Typography variant="h3" className="title green">
             <FormattedMessage id={storeAlerts.error.title} />
           </Typography>
         )}
         {storeAlerts.error.body && (
-          <Typography variant="body1" className="body red">
+          <Typography variant="body1" className="body green">
             <b>Error:</b>
             <FormattedMessage id={storeAlerts.error.body} />
           </Typography>
@@ -39,4 +39,4 @@ const ErrorAlert = () => {
   );
 };
 
-export default ErrorAlert;
+export default SuccessAlert;
