bf0fff5fa2e55568239e7e53be729b27c2502816
Murad Malik <muradmalik7@gmail.com>
2020-06-19 01:01:25 +0500

    ZIG-226: fixed crypto composition's strange behavior

--- src/components/Balance/CryptoComposition/CompositionGraph/CompositionGraph.js
+++ src/components/Balance/CryptoComposition/CompositionGraph/CompositionGraph.js
@@ -1,5 +1,4 @@
 import React from "react";
-import "./CompositionGraph.scss";
 import Doughnut from "../../../Graphs/Doughnut";
 import { useIntl } from "react-intl";
 
@@ -35,42 +34,49 @@ const CompositionGraph = ({ list, quotes }) => {
   ];
 
   /**
-   * @typedef {import("../../../Graphs/GradientLineChart/GradientLineChart").ChartData} ChartData
-   * @type {ChartData}
+   * @type {Array<Number>}
    */
-  let chartData = { values: [], labels: [] };
+  let values = [];
+
+  /**
+   * @type {Array<String>}
+   */
+  let labels = [];
 
   const colorsOptions = {
     backgroundColor: sectionColors,
   };
 
   const prepareChartData = () => {
+    let currentDate = new Date().getDate();
     /**
      * @type {*}
      */
-    let equity = list.length ? list[0] : {};
+    let equity = list.length
+      ? list.find((item) => new Date(item.date).getDate() === currentDate)
+      : {};
     if (equity) {
       for (let a = 0; a < quotes.length; a++) {
         let property = quotes[a] + "percentage";
         let value =
           typeof equity[property] === "string" ? parseFloat(equity[property]) : equity[property];
         if (value > 0) {
-          chartData.values.push(value.toFixed(2));
-          chartData.labels.push(quotes[a]);
+          values.push(value.toFixed(2));
+          labels.push(quotes[a]);
         }
       }
-      chartData.values.push(
+      values.push(
         typeof equity.otherPercentage === "string"
           ? parseFloat(equity.otherPercentage)
           : equity.otherPercentage,
       );
-      chartData.labels.push(intl.formatMessage({ id: "graph.others" }));
+      labels.push(intl.formatMessage({ id: "graph.others" }));
     }
   };
 
   prepareChartData();
 
-  return <Doughnut chartData={chartData} colorOptions={colorsOptions} />;
+  return <Doughnut colorOptions={colorsOptions} labels={labels} values={values} />;
 };
 
 export default CompositionGraph;
