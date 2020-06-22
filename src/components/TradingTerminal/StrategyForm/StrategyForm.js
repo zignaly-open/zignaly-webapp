b5009105c4c5ca1619ca188cbda77d8faa9e5b6e
Pablo Cerda <pablo@citla.com>
2020-06-22 19:52:08 +0200

    ZIG-424: Trading chart strategy price change monitor / re-draw.

--- src/components/TradingTerminal/StrategyForm/StrategyForm.js
+++ src/components/TradingTerminal/StrategyForm/StrategyForm.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Box, Button } from "@material-ui/core";
 import { useForm, FormContext } from "react-hook-form";
 import "./StrategyForm.scss";
@@ -13,12 +13,15 @@ import AutoclosePanel from "../AutoclosePanel/AutoclosePanel";
 /**
  * @typedef {import("../../../services/coinRayDataFeed").MarketSymbol} MarketSymbol
  * @typedef {import("../../../services/coinRayDataFeed").CoinRayCandle} CoinRayCandle
+ * @typedef {import("../../../tradingView/charting_library.min").IChartingLibraryWidget} TVWidget
+ * @typedef {import("../../../tradingView/charting_library.min").IPositionLineAdapter} TVChartLine
  */
 
 /**
  * @typedef {Object} StrategyFormProps
  * @property {Object} dataFeed
  * @property {CoinRayCandle} lastPriceCandle
+ * @property {TVWidget} tradingViewWidget
  * @property {number} leverage
  * @property {string} selectedSymbol
  */
@@ -30,7 +33,7 @@ import AutoclosePanel from "../AutoclosePanel/AutoclosePanel";
  * @returns {JSX.Element} Strategy form element.
  */
 const StrategyForm = (props) => {
-  const { dataFeed, lastPriceCandle, leverage, selectedSymbol } = props;
+  const { dataFeed, lastPriceCandle, leverage, selectedSymbol, tradingViewWidget } = props;
   const currentPrice = parseFloat(lastPriceCandle[1]).toFixed(8);
   const methods = useForm({
     mode: "onChange",
@@ -42,15 +45,88 @@ const StrategyForm = (props) => {
       entryType: "LONG",
     },
   });
+  const { getValues, setValue, watch } = methods;
+
+  /**
+   * @type {Object<String, TVChartLine|null>}
+   */
+  const defaultLinesTracking = {
+    price: null,
+    stopLossPrice: null,
+    trailingStopPrice: null,
+    dcaTargetPrice1: null,
+    profitTargetPrice1: null,
+  };
+  const [linesTracking, setLinesTracking] = useState(defaultLinesTracking);
+
+  /**
+   * @typedef {Object} ChartLineParams
+   * @property {string} id Line ID for tracking purposes for remove / redraw.
+   * @property {number} price Line price.
+   * @property {string} label Line label.
+   * @property {string} color Line color.
+   */
+
+  /**
+   * Draw price line at Trading View Chart.
+   *
+   * @param {ChartLineParams} chartLineParams Chart line parameters object.
+   * @returns {TVChartLine} TV chart line object.
+   */
+  function drawLine(chartLineParams) {
+    const { id, price, label, color } = chartLineParams;
+    const existingChartLine = linesTracking[id] || null;
+    // When line already exists, remove prior to draw to prevent duplication.
+    if (existingChartLine) {
+      existingChartLine.remove();
+    }
+
+    const chart = tradingViewWidget.chart();
+    const chartLine = chart
+      .createPositionLine({})
+      .setPrice(price)
+      .setQuantity(`${price}`)
+      .setText(label)
+      // horizontal line
+      .setLineColor(color)
+      // content text
+      .setBodyTextColor(color)
+      // content text border
+      .setBodyBorderColor(color)
+      // accompanying number
+      .setQuantityBackgroundColor(color)
+      // accompanying number border
+      .setQuantityBorderColor(color);
+
+    // Track the chart line object.
+    linesTracking[id] = chartLine;
+
+    return chartLine;
+  }
+
   // Receives submitted data.
   const onSubmit = () => {};
   // @ts-ignore
   const symbolsData = dataFeed.getSymbolsData();
   const updatePriceField = () => {
-    methods.setValue("price", currentPrice);
+    setValue("price", currentPrice);
+  };
+
+  const strategyPrice = watch("price");
+  const drawPriceLines = () => {
+    const linePriceColor = "rgb(117, 16, 197)";
+    drawLine({
+      id: "price",
+      price: parseFloat(strategyPrice),
+      label: "Price",
+      color: linePriceColor,
+    });
   };
 
   useEffect(updatePriceField, [currentPrice]);
+
+  console.log("Values: ", getValues());
+  useEffect(drawPriceLines, [strategyPrice]);
 
   /**
    * Match current symbol against market symbols collection item.
