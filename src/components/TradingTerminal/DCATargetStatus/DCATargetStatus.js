af0d7386b5a8ca014a58994a92e3159d16f544a8
Pablo Cerda <pablo@citla.com>
2020-07-01 23:16:53 +0200

    ZIG-701: DCA status / description component and styling.

---
+++ src/components/TradingTerminal/DCATargetStatus/DCATargetStatus.js
@@ -0,0 +1,58 @@
+import React from "react";
+import { FormattedMessage, useIntl } from "react-intl";
+import { colors } from "../../../services/theme";
+import { Box, FormHelperText, Tooltip } from "@material-ui/core";
+import { Help } from "@material-ui/icons";
+import "./DCATargetStatus.scss";
+
+/**
+ * @typedef {import("../../../services/tradeApiClient.types").ReBuyTarget} RebuyTarget
+ */
+
+/**
+ * @typedef {Object} DCATargetLabelProps
+ * @property {string} labelId Status label translation text ID.
+ * @property {RebuyTarget} dcaTarget Position take profit target.
+ */
+
+/**
+ * DCA status label with detailed description tooltip.
+ *
+ * @param {DCATargetLabelProps} props Component props.
+ * @returns {JSX.Element} Helper label with description in tooltip element.
+ */
+const DCATargetStatus = (props) => {
+  const { dcaTarget, labelId } = props;
+  const { formatMessage } = useIntl();
+  let iconColor = colors.purpleLight;
+
+  if (!dcaTarget) {
+    return null;
+  }
+
+  console.log("dcaTarget: ", dcaTarget);
+  let description = dcaTarget.orderId
+    ? formatMessage({ id: "terminal.status.placed" }, { orderId: dcaTarget.orderId })
+    : formatMessage({ id: "terminal.status.pending" });
+
+  if (dcaTarget.done) {
+    description = formatMessage({ id: "terminal.status.done" });
+    iconColor = colors.green;
+  } else if (dcaTarget.skipped) {
+    description = dcaTarget.errorMSG || formatMessage({ id: "terminal.status.failed" });
+    iconColor = colors.red;
+  }
+
+  return (
+    <Box alignItems="center" className="targetStatus" display="flex" justifyContent="flex-end">
+      <FormHelperText>
+        <FormattedMessage id={labelId} />
+      </FormHelperText>
+      <Tooltip arrow enterTouchDelay={50} placement="left-end" title={description}>
+        <Help style={{ fill: iconColor }} />
+      </Tooltip>
+    </Box>
+  );
+};
+
+export default React.memo(DCATargetStatus);
