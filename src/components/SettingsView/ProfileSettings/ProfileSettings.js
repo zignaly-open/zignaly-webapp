192ef36ba99a816b4264a70d23d6d5db0afead66
Christophe Wagner <chris.wagn22@gmail.com>
2020-07-07 00:01:17 +0700

    ZIG-462: Add profile tab

--- src/components/SettingsView/ProfileSettings/ProfileSettings.js
+++ src/components/SettingsView/ProfileSettings/ProfileSettings.js
@@ -1,63 +1,66 @@
-// import React from "react";
-// import { Box, Checkbox, FormControlLabel } from "@material-ui/core";
-// import { FormattedMessage } from "react-intl";
-// import { useDispatch } from "react-redux";
-// import { toggleBalanceBox } from "../../../store/actions/settings";
-// import useStoreSettingsSelector from "../../../hooks/useStoreSettingsSelector";
-// import ThemeSwitcher from "../../ThemeSwitcher";
-// import "./ProfileSettings.scss";
-// import CustomButton from "../../CustomButton";
-// import tradeApi from "../../../services/tradeApiClient";
-// import { showErrorAlert, showSuccessAlert } from "../../../store/actions/ui";
-// import { useForm, Controller } from "react-hook-form";
+import React from "react";
+import { Box, Typography, OutlinedInput } from "@material-ui/core";
+import { FormattedMessage } from "react-intl";
+import { useStoreUserData } from "../../../hooks/useStoreUserSelector";
+import "./ProfileSettings.scss";
 
-// const ProfileSettings = () => {
-//   const dispatch = useDispatch();
-//   const storeSettings = useStoreSettingsSelector();
-//   const { handleSubmit, register, reset, control, errors } = useForm();
-//   const storeSession = useStoreSessionSelector();
+const ProfileSettings = () => {
+  //   const dispatch = useDispatch();
+  const storeUserData = useStoreUserData();
+  //   const { handleSubmit, register, reset, control, errors } = useForm();
+  //   const storeSession = useStoreSessionSelector();
 
-//   /**
-//    * @typedef {Object} FormData
-//    * @property {string} email
-//    */
+  //   /**
+  //    * @typedef {Object} FormData
+  //    * @property {string} email
+  //    */
 
-//   /**
-//    * Function to submit form.
-//    *
-//    * @param {FormData} data Form data.
-//    * @returns {void}
-//    */
-//   const submitForm = (data) => {
-//     const payload = {
-//       token: storeSession.tradeApi.accessToken,
-//     };
+  //   /**
+  //    * Function to submit form.
+  //    *
+  //    * @param {FormData} data Form data.
+  //    * @returns {void}
+  //    */
+  //   const submitForm = (data) => {
+  //     const payload = {
+  //       token: storeSession.tradeApi.accessToken,
+  //     };
 
-//     setUpdating(true);
+  //     setUpdating(true);
 
-//     tradeApi
-//       .updateProfileNotifications(payload)
-//       .then((data) => {
-//         dispatch(showSuccessAlert("Success", "accounts.settings.saved"));
-//       })
-//       .catch((e) => {
-//         dispatch(showErrorAlert(e));
-//       })
-//       .finally(() => {
-//         setUpdating(false);
-//       });
-//   };
+  //     tradeApi
+  //       .updateProfileNotifications(payload)
+  //       .then((data) => {
+  //         dispatch(showSuccessAlert("Success", "accounts.settings.saved"));
+  //       })
+  //       .catch((e) => {
+  //         dispatch(showErrorAlert(e));
+  //       })
+  //       .finally(() => {
+  //         setUpdating(false);
+  //       });
+  //   };
 
-//   return (
-//     <form onSubmit={handleSubmit(submitForm)}>
-//       <Box
-//         alignItems="flex-start"
-//         className="profileSettings"
-//         display="flex"
-//         flexDirection="column"
-//       ></Box>
-//     </form>
-//   );
-// };
+  return (
+    // <form onSubmit={handleSubmit(submitForm)}></form>
+    <Box alignItems="flex-start" className="profileSettings" display="flex" flexDirection="column">
+      <label className="inputBox">
+        <Typography variant="body1">
+          <FormattedMessage id="profile.userid" />
+        </Typography>
+        <Typography variant="body1" className="userId">
+          {storeUserData.userId}
+        </Typography>
+      </label>
+      <label className="inputBox">
+        <Typography variant="body1">
+          <FormattedMessage id="profile.email" />
+        </Typography>
+        <OutlinedInput className="customInput" disabled={true} value={storeUserData.email} />
+      </label>
+    </Box>
+    // </form>
+  );
+};
 
-// export default ProfileSettings;
+export default ProfileSettings;
