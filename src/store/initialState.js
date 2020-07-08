ac72156784cf6ecf1dd186479ba6ab7de768bfbf
Pablo Cerda <pablo@citla.com>
2020-07-08 21:21:28 +0200

    ZIG-783: Add openEditPositions cols display default configuration.

--- src/store/initialState.js
+++ src/store/initialState.js
@@ -174,6 +174,17 @@ const initialState = {
         "col.invested",
         "col.actions",
       ],
+      openEditPositions: [
+        "col.date.open",
+        "col.stat",
+        "col.provider.logo",
+        "col.pair",
+        "col.price.current",
+        "col.plnumber",
+        "col.plpercentage",
+        "col.invested",
+        "col.actions",
+      ],
       closedPositions: [
         "col.paper",
         "col.date.open",
