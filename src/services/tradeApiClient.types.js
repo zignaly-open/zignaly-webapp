26c4cbcc24b254911b7bd948c5e4fded46892528
Murad Malik <muradmalik7@gmail.com>
2020-07-16 18:39:39 +0500

    valid until is now in users timezone

--- src/services/tradeApiClient.types.js
+++ src/services/tradeApiClient.types.js
@@ -3304,6 +3304,6 @@ const createEmptyUserExchangeAssetsEntity = () => {
 export function sessionDataResponseTransform(response) {
   return {
     status: response.status,
-    validUntil: response.validUntil * 1000,
+    validUntil: new Date(response.validUntil * 1000).getTime(),
   };
 }
