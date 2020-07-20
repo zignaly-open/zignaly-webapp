1e5a0bef16d8f1d610317cc132b9575bf1e76bf3
Pablo Cerda <pablo@citla.com>
2020-07-21 00:19:22 +0200

    ZIG-927: Replace freshDesk by ZenDesk customer support widget.

---
+++ static/widgets/customerSupportWidget.js
@@ -0,0 +1,21 @@
+window.zESettings = {
+  webWidget: {
+    position: { horizontal: "right", vertical: "bottom" },
+    offset: {
+      horizontal: "0",
+      vertical: "20px",
+      mobile: {
+        horizontal: "-10px",
+        vertical: "50px",
+      },
+    },
+  },
+};
+
+var script = document.createElement("script");
+script.type = "text/javascript";
+script.src = "https://static.zdassets.com/ekr/snippet.js?key=374628c3-7648-4afb-96c4-c13bc6408a8f";
+script.id = "ze-snippet";
+script.async = true;
+script.defer = true;
+document.getElementsByTagName("head")[0].appendChild(script);
