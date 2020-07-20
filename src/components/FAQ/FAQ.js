dd7420e93a9ee2298d621c671008b7d0d75068eb
Murad Malik <muradmalik7@gmail.com>
2020-07-20 16:03:59 +0500

    ZIG-892: hover state implemented for menus and tabs

--- src/components/FAQ/FAQ.js
+++ src/components/FAQ/FAQ.js
@@ -1,55 +1,57 @@
 import React from "react";
 import "./FAQ.scss";
-import { Box, Typography } from "@material-ui/core";
-import { FormattedMessage } from "react-intl";
-import DocsIcon from "../../images/documents.svg";
+import { Box } from "@material-ui/core";
+// import { Box, Typography } from "@material-ui/core";
+// import { FormattedMessage } from "react-intl";
+// import DocsIcon from "../../images/documents.svg";
 
 const FAQ = () => {
-  return (
-    <Box
-      alignItems="flex-start"
-      className="faq"
-      display="flex"
-      flexDirection="column"
-      justifyContent="flex-start"
-    >
-      <Typography className="title" variant="h4">
-        <FormattedMessage id="faq" />
-      </Typography>
-      <Box
-        alignItems="center"
-        className="questionContainer"
-        display="flex"
-        flexDirection="row"
-        justifyContent="space-between"
-      >
-        <Typography className="question" variant="caption">
-          <FormattedMessage id="faq.question1" />
-        </Typography>
-        <Typography className="question" variant="caption">
-          <FormattedMessage id="faq.question2" />
-        </Typography>
-        <Typography className="question" variant="caption">
-          <FormattedMessage id="faq.question3" />
-        </Typography>
-        <Typography className="question" variant="caption">
-          <FormattedMessage id="faq.question4" />
-        </Typography>
-        <Box
-          alignItems="center"
-          className="docsBox"
-          display="flex"
-          flexDirection="row"
-          justifyContent="flex-start"
-        >
-          <img alt="Zignaly-Docs" className="docsIcon" src={DocsIcon} />
-          <Typography className="docs" variant="h6">
-            <FormattedMessage id="faq.documentation" />
-          </Typography>
-        </Box>
-      </Box>
-    </Box>
-  );
+  return <Box></Box>;
+  // return (
+  //   <Box
+  //     alignItems="flex-start"
+  //     className="faq"
+  //     display="flex"
+  //     flexDirection="column"
+  //     justifyContent="flex-start"
+  //   >
+  //     <Typography className="title" variant="h4">
+  //       <FormattedMessage id="faq" />
+  //     </Typography>
+  //     <Box
+  //       alignItems="center"
+  //       className="questionContainer"
+  //       display="flex"
+  //       flexDirection="row"
+  //       justifyContent="space-between"
+  //     >
+  //       <Typography className="question" variant="caption">
+  //         <FormattedMessage id="faq.question1" />
+  //       </Typography>
+  //       <Typography className="question" variant="caption">
+  //         <FormattedMessage id="faq.question2" />
+  //       </Typography>
+  //       <Typography className="question" variant="caption">
+  //         <FormattedMessage id="faq.question3" />
+  //       </Typography>
+  //       <Typography className="question" variant="caption">
+  //         <FormattedMessage id="faq.question4" />
+  //       </Typography>
+  //       <Box
+  //         alignItems="center"
+  //         className="docsBox"
+  //         display="flex"
+  //         flexDirection="row"
+  //         justifyContent="flex-start"
+  //       >
+  //         <img alt="Zignaly-Docs" className="docsIcon" src={DocsIcon} />
+  //         <Typography className="docs" variant="h6">
+  //           <FormattedMessage id="faq.documentation" />
+  //         </Typography>
+  //       </Box>
+  //     </Box>
+  //   </Box>
+  // );
 };
 
 export default FAQ;
