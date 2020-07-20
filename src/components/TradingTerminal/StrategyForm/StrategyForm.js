bbc31c6a9a40c76ed73c66b7a0c2cafebab36c4d
Pablo Cerda <pablo@citla.com>
2020-07-20 21:48:37 +0200

    ZIG-900: Fix lint error: debug console.log.

--- src/components/TradingTerminal/StrategyForm/StrategyForm.js
+++ src/components/TradingTerminal/StrategyForm/StrategyForm.js
@@ -417,7 +417,6 @@ const StrategyForm = (props) => {
 
   // @ts-ignore
   const updatePriceField = () => {
-    console.log("lastPrice: ", lastPrice);
     setValue("price", lastPrice);
     // Hidden price input used when strategy panels is collapsed due to the fact
     // that unmounted input value is removed from form state.
