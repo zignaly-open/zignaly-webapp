e764a85f43f4ca89544299ef895037a343b0c58f
Christophe Wagner <chris.wagn22@gmail.com>
2020-07-23 16:02:45 +0700

    ZIIG-920: Auto refresh profile open positions.

--- src/hooks/usePositionsList.js
+++ src/hooks/usePositionsList.js
@@ -223,7 +223,7 @@ const usePositionsList = (type, positionEntity = null) => {
 
   const updateData = () => {
     // Only open positions needs continuos updates.
-    if (type === "open") {
+    if (type === "open" || type === "profileOpen") {
       // Single position update.
       if (positionEntity) {
         loadPosition();
