9495b823c16d50295bc9485d8db521f762366764
Pablo Cerda <pablo@citla.com>
2020-08-03 21:16:20 +0200

    ZIG-1053: Fix semantic-release config file parse errors.

--- release.config.js
+++ release.config.js
@@ -1,24 +1,25 @@
-{
-  branches: [
-    {name: 'master'},
-    {name: 'develop', channel: 'beta', prerelease: 'beta'},
-  ],
+module.exports = {
+  branches: [{ name: "master" }, { name: "develop", channel: "beta", prerelease: "beta" }],
   plugins: [
-    ["@semantic-release/commit-analyzer", {
-      "preset": "angular",
-      "releaseRules": [
-        {"type": "refactor", "release": "patch"},
-        {"type": "style", "release": "patch"}
-      ],
-      "parserOpts": {
-        "noteKeywords": ["BREAKING"]
-      }
-    }],
+    [
+      "@semantic-release/commit-analyzer",
+      {
+        preset: "angular",
+        releaseRules: [
+          { type: "refactor", release: "patch" },
+          { type: "style", release: "patch" },
+        ],
+        parserOpts: {
+          noteKeywords: ["BREAKING"],
+        },
+      },
+    ],
     "@semantic-release/release-notes-generator",
-    ["@semantic-release/github", {
-      "assets": [
-        {"path": "public/*", "label": "Zignaly webapp distribution"},
-      ]
-    }]
-  ]
-}
+    [
+      "@semantic-release/github",
+      {
+        assets: [{ path: "public/*", label: "Zignaly webapp distribution" }],
+      },
+    ],
+  ],
+};
