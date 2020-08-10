0e93a120c3b5eea4a7f3dd7911829355d7801bca
Pablo Cerda <pablo@citla.com>
2020-08-10 22:09:25 +0200

    feat: ZIG-936 Restore filters state after extended status results renders

--- src/components/Dashboard/PositionFilters/PositionFilters.js
+++ src/components/Dashboard/PositionFilters/PositionFilters.js
@@ -9,10 +9,15 @@ import { Box } from "@material-ui/core";
 /**
  * @typedef {import("../../../services/tradeApiClient.types").UserPositionsCollection} UserPositionsCollection
  * @typedef {import("react").MouseEventHandler} MouseEventHandler
+ * @typedef {import("../../../hooks/usePositionsList").PositionsFiltersState} PositionsFiltersState
+ */
+
+/**
  * @typedef {Object} PositionFiltersPropTypes
  * @property {Function} onChange Callback to broadcast filters changes to caller.
- * @property {UserPositionsCollection} positions
- * @property {boolean} showTypesFilter
+ * @property {UserPositionsCollection} positions Positions collection.
+ * @property {PositionsFiltersState} initialState Filters initial state.
+ * @property {boolean} showTypesFilter Flag to indicate whether types dropdown filter display or not.
  */
 
 /**
@@ -22,15 +27,15 @@ import { Box } from "@material-ui/core";
  * @returns {JSX.Element} Component JSX.
  */
 const PositionFilters = (props) => {
-  const { onChange, positions, showTypesFilter } = props;
+  const { initialState, onChange, positions, showTypesFilter } = props;
   const defaultFilters = {
-    providerName: "all",
+    provider: "all",
     pair: "all",
     side: "all",
     type: "all",
     status: "",
   };
-  const [filters, setFilters] = useState(defaultFilters);
+  const [filters, setFilters] = useState(initialState);
 
   const extractPairOptions = () => {
     const coinsDistinct = uniqBy(positions, "pair").map((position) => {
@@ -80,7 +85,7 @@ const PositionFilters = (props) => {
   const setProvider = (value) => {
     setFilters({
       ...filters,
-      providerName: value,
+      provider: value,
     });
   };
 
@@ -148,7 +153,7 @@ const PositionFilters = (props) => {
         label=""
         onChange={setProvider}
         options={providerOptions}
-        value={filters.providerName}
+        value={filters.provider}
       />
       <CustomSelect label="" onChange={setCoin} options={pairOptions} value={filters.pair} />
       <CustomSelect label="" onChange={setSide} options={sides} value={filters.side} />
