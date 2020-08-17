3f14ef164953ef7252020f5104d6c29167c4eb3f
Pablo Cerda <pablo@citla.com>
2020-08-17 13:49:07 +0200

    feat: ZIG-1209 Custom GTM signup event tracking

--- src/store/actions/session.js
+++ src/store/actions/session.js
@@ -4,6 +4,7 @@ import { showErrorAlert } from "./ui";
 import { isEmpty } from "lodash";
 import { navigate } from "gatsby";
 import tradeApi from "../../services/tradeApiClient";
+import gtmPushApi from "../../utils/gtmPushApi";
 
 export const START_TRADE_API_SESSION = "START_TRADE_API_SESSION";
 export const END_TRADE_API_SESSION = "END_TRADE_API_SESSION";
@@ -65,9 +66,23 @@ export const endTradeApiSession = () => {
  * @returns {AppThunk} Thunk action function.
  */
 export const registerUser = (payload, setLoading) => {
+  const { gtmEventPush } = gtmPushApi();
+  const eventData = {
+    event: "signup",
+  };
+
   return async (dispatch) => {
     try {
       const responseData = await tradeApi.userRegister(payload);
+      // TODO: webapp1 is passing user data to the event but based on
+      // https://support.google.com/tagmanager/answer/7679219 I don't see
+      // that this data is really tracked so will check with David
+      // and may violate GDRP regulation if we don't ask user consent.
+      // Object.keys(responseData).forEach((key) => {
+      //   eventData[key] = responseData[key];
+      // });
+
+      gtmEventPush(eventData);
       dispatch(startTradeApiSession(responseData));
       setLoading(false);
     } catch (e) {
