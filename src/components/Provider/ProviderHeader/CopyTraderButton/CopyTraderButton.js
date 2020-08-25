f61a42967ea6103bdd85c00af3bd5e6711d4902c
Christophe Wagner <chris.wagn22@gmail.com>
2020-08-25 20:33:38 +0700

    fix: Fix lint error

--- src/components/Provider/ProviderHeader/CopyTraderButton/CopyTraderButton.js
+++ src/components/Provider/ProviderHeader/CopyTraderButton/CopyTraderButton.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import "./CopyTraderButton.scss";
 import { Box, Typography, Tooltip } from "@material-ui/core";
 import CustomButton from "../../../CustomButton";
--- src/components/Provider/ProviderHeader/FollowProviderButton/FollowProviderButton.js
+++ src/components/Provider/ProviderHeader/FollowProviderButton/FollowProviderButton.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import "./FollowProviderButton.scss";
 import { Box, Typography, Tooltip } from "@material-ui/core";
 import CustomButton from "../../../CustomButton";
