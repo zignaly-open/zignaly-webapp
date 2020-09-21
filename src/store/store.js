399518ade6967f5a87cbab223accbb6406cd272e
Christophe Wagner <chris.wagn22@gmail.com>
2020-09-21 10:39:59 +0700

    fix: ZIG-1358 Fix default sort providers

--- src/store/store.js
+++ src/store/store.js
@@ -42,13 +42,18 @@ const migrations = {
     state.settings.sort.signalp = "";
     return state;
   },
+  13: (/** @type {PersistedState} */ state) => {
+    // @ts-ignore
+    state.settings.sort.signalp = "";
+    return state;
+  },
 };
 
 const persistConfig = {
   key: "zignaly-webapp2",
   storage,
   stateReconciler: autoMergeLevel2,
-  version: 12,
+  version: 13,
   migrate: createMigrate(migrations, { debug: false }),
   blacklist: ["ui"],
 };
