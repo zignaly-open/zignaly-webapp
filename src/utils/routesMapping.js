14e777acdc302dc3905c244d6125a1c7e1356037
Christophe Wagner <chris.wagn22@gmail.com>
2020-09-25 23:07:40 +0700

    fix: ZIG-1081 Remove wall flag

--- src/utils/routesMapping.js
+++ src/utils/routesMapping.js
@@ -248,7 +248,7 @@ export const createProviderRoutes = (providerId, provider, selectedExchange) => 
       ],
     };
 
-    if (process.env.GATSBY_ENABLE_FEED.toLowerCase() === "true" && !provider.isClone) {
+    if (!provider.isClone) {
       data.links.push({
         id: "srv.newsfeed",
         to: `/signalProviders/${provider.id}/feed`,
