546e3fd940b245fe67384fa11b4edb5639e779c5
Christophe Wagner <chris.wagn22@gmail.com>
2020-09-29 22:40:37 +0700

    fix: ZIG-1437 Fix infinite login redirections

--- src/hooks/useRedirectUponSessionValid.js
+++ src/hooks/useRedirectUponSessionValid.js
@@ -1,12 +1,23 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import useStoreSessionSelector from "./useStoreSessionSelector";
 import { navigate } from "gatsby";
 import { verifySessionData } from "../utils/auth";
+import { globalHistory } from "@reach/router";
 
 const useRedirectUponSessionValid = () => {
   const storeSession = useStoreSessionSelector();
+  const forced = useRef(globalHistory.location.state && globalHistory.location.state.forced);
 
   useEffect(() => {
+    // Don't redirect to private area if we've been forced to redirect already.
+    // This happens when the tradeapi client detects the session has expired. It's not possible to
+    // clear the auth token there, we should detect it as expired now but there is a small chance
+    // that's not the case, at least locally, when switching api server for example.
+    if (forced.current) {
+      forced.current = false;
+      return;
+    }
+
     if (verifySessionData(storeSession.tradeApi.accessToken, storeSession.sessionData)) {
       // Navigate to return url or dashboard
       const params = new URLSearchParams(
@@ -17,7 +28,6 @@ const useRedirectUponSessionValid = () => {
       const pathWithoutPrefix = path.replace(pathPrefix, "");
       navigate(pathWithoutPrefix);
     }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [storeSession.sessionData]);
 };
 
