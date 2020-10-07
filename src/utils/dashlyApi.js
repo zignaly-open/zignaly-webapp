60e3f1d90d2f7d241a06c4488e14fc5e56aec1bf
Murad Malik <muradmalik7@gmail.com>
2020-10-07 14:59:52 +0500

    feat: dashly api created

--- src/utils/dashlyApi.js
+++ src/utils/dashlyApi.js
@@ -1,4 +1,5 @@
 /**
+ * @typedef {import("../services/tradeApiClient.types").UserEntity} UserEntity
  * @typedef {Object} DashlyApi
  * @property {Object} dashly GTM API push function.
  */
@@ -27,3 +28,89 @@ const dashlyApi = () => {
 };
 
 export default dashlyApi;
+
+/**
+ * @param {UserEntity} userData User Entity.
+ * @returns {void} None.
+ */
+export const dashlyLogin = (userData) => {
+  const { dashly } = dashlyApi();
+
+  if (dashly) {
+    // @ts-ignore
+    dashly.auth(userData.userId, userData.dashlyHash);
+
+    // @ts-ignore
+    dashly.track("$authorized", {
+      $email: userData.email,
+      $name: userData.firstName,
+      subscribed: userData.subscribe,
+      created: userData.createdAt,
+    });
+
+    // @ts-ignore
+    dashly.onReady(() => {
+      // @ts-ignore
+      dashly.identify([
+        { op: "update_or_create", key: "$email", value: userData.email },
+        { op: "update_or_create", key: "$name", value: userData.firstName },
+      ]);
+
+      // @ts-ignore
+      dashly.identify([
+        { op: "update_or_create", key: "ref", value: userData.ref },
+        { op: "update_or_create", key: "subscribed", value: userData.subscribe },
+        { op: "update_or_create", key: "created", value: userData.createdAt },
+        { op: "update_or_create", key: "2fa", value: userData.twoFAEnable },
+        { op: "update_or_create", key: "providers_enabled", value: userData.providerEnable },
+        { op: "update_or_create", key: "exchange_connected", value: userData.binanceConnected },
+        { op: "update_or_create", key: "buys_count", value: userData.buysCount },
+        { op: "update_or_create", key: "sells_count", value: userData.sellsCount },
+        { op: "update_or_create", key: "status", value: userData.status },
+      ]);
+    });
+  }
+};
+
+/**
+ * @param {UserEntity} userData User Entity.
+ * @returns {void} None.
+ */
+export const dashlyRegister = (userData) => {
+  const { dashly } = dashlyApi();
+
+  if (dashly) {
+    // @ts-ignore
+    dashly.auth(userData.userId, userData.dashlyHash);
+
+    // @ts-ignore
+    dashly.track("$registered", {
+      $email: userData.email,
+      $name: userData.firstName,
+      subscribed: userData.subscribe,
+      created: userData.createdAt,
+    });
+
+    // @ts-ignore
+    dashly.onReady(() => {
+      // @ts-ignore
+      dashly.identify([
+        { op: "update_or_create", key: "$email", value: userData.email },
+        { op: "update_or_create", key: "$name", value: userData.firstName },
+      ]);
+
+      // @ts-ignore
+      dashly.identify([
+        { op: "update_or_create", key: "ref", value: userData.ref },
+        { op: "update_or_create", key: "subscribed", value: userData.subscribe },
+        { op: "update_or_create", key: "created", value: userData.createdAt },
+        { op: "update_or_create", key: "2fa", value: userData.twoFAEnable },
+        { op: "update_or_create", key: "providers_enabled", value: userData.providerEnable },
+        { op: "update_or_create", key: "exchange_connected", value: userData.binanceConnected },
+        { op: "update_or_create", key: "buys_count", value: userData.buysCount },
+        { op: "update_or_create", key: "sells_count", value: userData.sellsCount },
+        { op: "update_or_create", key: "status", value: userData.status },
+      ]);
+    });
+  }
+};
