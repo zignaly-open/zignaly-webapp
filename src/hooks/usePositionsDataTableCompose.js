e7c16edb8ae37eab7e64ba801b10730a30e742b7
Murad Malik <muradmalik7@gmail.com>
2020-10-13 00:22:57 +0500

    fix: fixed js types

--- src/hooks/usePositionsDataTableCompose.js
+++ src/hooks/usePositionsDataTableCompose.js
@@ -660,7 +660,7 @@ export function usePositionDataTableCompose(positions, confirmActionHandler) {
    */
   function renderAllActionButtons(dataIndex) {
     const position = positions[dataIndex];
-    if(position.profitSharing){
+    if (position.profitSharing) {
       return null;
     }
     return composeAllActionButtons(position, confirmActionHandler);
