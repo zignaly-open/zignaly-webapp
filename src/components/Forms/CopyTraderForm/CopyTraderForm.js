bbde88041bd8c2b023b78a04e60e8b49e14cd3cd
Murad Malik <muradmalik7@gmail.com>
2020-10-14 15:29:38 +0500

    fix: fixed copying a trader from demo account

--- src/components/Forms/CopyTraderForm/CopyTraderForm.js
+++ src/components/Forms/CopyTraderForm/CopyTraderForm.js
@@ -116,7 +116,7 @@ const CopyTraderForm = ({ provider, onClose }) => {
 
   const validateExchange = () => {
     if (storeUserExchangeConnections.length > 0) {
-      if (storeSettings.selectedExchange.paperTrading) {
+      if (provider.profitSharing && storeSettings.selectedExchange.paperTrading) {
         let msg = intl.formatMessage({ id: "copyt.copy.error4" });
         setAlert(msg);
         return false;
