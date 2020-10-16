d510b9a86fbda87bfd372c7b19de35b3365fe060
Christophe Wagner <chris.wagn22@gmail.com>
2020-10-16 16:16:54 +0200

    feat: ZIG-1551 Add chart missing days

--- src/utils/chart.js
+++ src/utils/chart.js
@@ -1,5 +1,51 @@
-export const generateDays = (count) => {
-  for (let i = 0; i < count; i++) {
-    const element = array[i];
-  }
+import moment from "moment";
+
+/**
+ *
+ * @param {Array<*>} dailyData
+ * @param {function} addValue
+ * @returns {Array<*>}
+ */
+export const generateDailyData = (dailyData, addValue) => {
+  let res = [];
+
+  /**
+   * Fill missing days with passed amount value
+   * @param {number} daysCount
+   * @param {Date} afterDate
+   * @param {numer} amount
+   * @returns {void}
+   */
+  const generateMissingDays = (daysCount, afterDate, amount) => {
+    for (let i = 0; i < daysCount; i++) {
+      const date = moment(afterDate).add(i + 1, "d");
+      res.push(addValue(date, amount));
+    }
+  };
+
+  dailyData.reduce((/** @type {{date: Date, amount: number}} */ aggregatedData, currentData) => {
+    const currentDate = currentData.date;
+    let amount = currentData.amount;
+
+    if (aggregatedData) {
+      amount += aggregatedData.amount;
+      const daysDiff = moment(currentData.date).diff(moment(aggregatedData.date), "days");
+
+      if (daysDiff > 1) {
+        // Adding missing days
+        generateMissingDays(daysDiff - 1, aggregatedData.date, aggregatedData.amount);
+      }
+    }
+
+    // Add calculated daily amount
+    res.push(addValue(currentDate, amount));
+
+    // Return last aggregated data
+    return {
+      date: currentDate,
+      amount: amount,
+    };
+  }, null);
+
+  return res;
 };
