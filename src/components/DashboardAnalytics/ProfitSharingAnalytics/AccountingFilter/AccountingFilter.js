d547c3179a025c2726b3713e8b185ed68b791875
Christophe Wagner <chris.wagn22@gmail.com>
2020-10-22 21:27:51 +0200

    feat: ZIG-1489 Add filter to accounting table

---
+++ src/components/DashboardAnalytics/ProfitSharingAnalytics/AccountingFilter/AccountingFilter.js
@@ -0,0 +1,68 @@
+import React, { useState } from "react";
+import { Box } from "@material-ui/core";
+import CustomSelect from "../../../CustomSelect";
+import { useIntl } from "react-intl";
+/**
+ *
+ * @typedef {import("../../../../services/tradeApiClient.types").ProfitSharingBalanceEntry} ProfitSharingBalanceEntry
+ * @typedef {import("../../../CustomSelect/CustomSelect").OptionType} OptionType
+ */
+
+/**
+ *
+ * @typedef {Object} DefaultProps
+ * @property {Array<ProfitSharingBalanceEntry>} data
+ * @property {function(Array<ProfitSharingBalanceEntry>):*} onChange
+ * @property {Array<OptionType>} types
+ */
+
+/**
+ *
+ * @param {DefaultProps} props Default props.
+ */
+
+const AccountingFilter = ({ data, onChange, types }) => {
+  const [selected, setSelected] = useState("all");
+  const intl = useIntl();
+
+  /**
+   * Select change handler.
+   *
+   * @param {string} value Change event.
+   *
+   * @returns {Void} No return.
+   */
+  const handleChange = (value) => {
+    setSelected(value);
+    onChange(filterData(value));
+  };
+
+  /**
+   * Filter accounting data
+   *
+   * @param {string} value
+   * @returns {Array<ProfitSharingBalanceEntry>}
+   */
+
+  const filterData = (value) => {
+    if (value === "all") {
+      return data;
+    }
+
+    let filteredData = data.filter((item) => item.type === value);
+    return filteredData;
+  };
+
+  return (
+    <Box alignItems="center" className="accountFilter" display="flex" flexDirection="row">
+      <CustomSelect
+        label={intl.formatMessage({ id: "dashboard.balance.show" })}
+        onChange={handleChange}
+        options={types}
+        value={selected}
+      />
+    </Box>
+  );
+};
+
+export default AccountingFilter;
