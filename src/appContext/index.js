5deecd8540d541b366f327ba02a58e2d70f96b7b
Murad Malik <muradmalik7@gmail.com>
2020-11-27 01:44:22 +0500

    fix: removed app context and added provider context

--- src/appContext/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-/**
- * @typedef {Object} AppContextObject
- * @property {boolean} emptySettingsAlert
- * @property {React.SetStateAction<*>} setEmptySettingsAlert
- */
-
-export default React.createContext(/** @type {Partial<AppContextObject>} **/ ({}));
