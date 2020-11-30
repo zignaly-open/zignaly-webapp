594e9dc05f4ba84a81d1b9e2630c87f58eddd69f
Murad Malik <muradmalik7@gmail.com>
2020-12-01 00:48:43 +0500

    feat: usr report added

--- static/widgets/externalWidgets.js
+++ static/widgets/externalWidgets.js
@@ -1,3 +1,5 @@
+// zendesk
+
 window.zESettings = {
   webWidget: {
     position: { horizontal: "right", vertical: "bottom" },
@@ -33,6 +35,7 @@ script.async = true;
 script.defer = true;
 document.getElementsByTagName("head")[0].appendChild(script);
 
+// dashly
 let script2 = document.createElement("script");
 script2.type = "text/javascript";
 script2.src = "https://cdn.dashly.app/api.min.js";
@@ -40,9 +43,23 @@ script2.id = "dashly";
 script2.async = true;
 document.getElementsByTagName("head")[0].appendChild(script2);
 
+// user pilot
 let script3 = document.createElement("script");
 script3.type = "text/javascript";
 script3.src = "https://js.userpilot.io/sdk/latest.js";
 script3.id = "userPilot";
 script3.async = true;
 document.getElementsByTagName("head")[0].appendChild(script3);
+
+// user report
+let meta1 = document.createElement("meta");
+meta1.name = "userreport:mediaId";
+meta1.value = "66375182-a207-4c0a-9c18-716188f4581c";
+document.getElementsByTagName("head")[0].appendChild(meta1);
+
+let script4 = document.createElement("script");
+script4.type = "text/javascript";
+script4.src = "https://sak.userreport.com/zignaly/launcher.js";
+script4.id = "userReport";
+script4.async = true;
+document.getElementsByTagName("head")[0].appendChild(script4);
