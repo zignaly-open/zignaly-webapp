2d753885427612ddb1e8aa64b2a8220ad7180ac9
Christophe Wagner <chris.wagn22@gmail.com>
2020-12-04 18:58:33 +0100

    feat: Add polish langage

--- src/i18n/translations/index.js
+++ src/i18n/translations/index.js
@@ -2,10 +2,11 @@ import en from "./en.yml";
 import cs from "./cs.yml";
 import vn from "./vn.yml";
 import de from "./de.yml";
+import pl from "./pl.yml";
 
 /**
  * @type {Object<string, any>} translations
  */
-const translations = { cs, en, vn, de };
+const translations = { cs, en, vn, de, pl };
 
 export default translations;
