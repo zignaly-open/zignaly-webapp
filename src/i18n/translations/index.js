93f85cdbad9d7b71f060145268e443fcead6d33e
Murad Malik <muradmalik7@gmail.com>
2020-12-30 01:08:50 +0500

    feat: spanish lang added in test

--- src/i18n/translations/index.js
+++ src/i18n/translations/index.js
@@ -3,10 +3,11 @@ import cs from "./cs.yml";
 import vn from "./vn.yml";
 import de from "./de.yml";
 import pl from "./pl.yml";
+import es from "./es.yml";
 
 /**
  * @type {Object<string, any>} translations
  */
-const translations = { cs, en, vn, de, pl };
+const translations = { cs, en, vn, de, pl, es };
 
 export default translations;
