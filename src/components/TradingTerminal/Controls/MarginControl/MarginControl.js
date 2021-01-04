8cf5dfc017caed08cf098d656bcf0adc6f20f1a4
Christophe Wagner <chris.wagn22@gmail.com>
2021-01-04 18:49:27 +0100

    fix: Move cross selector

---
+++ src/components/TradingTerminal/Controls/MarginControl/MarginControl.js
@@ -0,0 +1,97 @@
+import React, { useState } from "react";
+import { Box, Button, Switch, Typography } from "@material-ui/core";
+import { FormattedMessage } from "react-intl";
+import { useFormContext } from "react-hook-form";
+import LeverageForm from "../../LeverageForm/LeverageForm";
+import Modal from "../../../Modal";
+import useStoreSettingsSelector from "hooks/useStoreSettingsSelector";
+import HelperLabel from "../../HelperLabel/HelperLabel";
+import "./MarginControl.scss";
+
+/**
+ * @typedef {import("services/coinRayDataFeed").MarketSymbol} MarketSymbol
+ */
+
+/**
+ * Manual trading strategy panel component.
+ *
+ * @param {Object} props Props.
+ * @param {MarketSymbol} props.symbolData Selected Symbol.
+ * @returns {JSX.Element} JSX.
+ */
+const MarginControl = ({ symbolData }) => {
+  const { register, watch, setValue } = useFormContext();
+  const leverage = watch("leverage");
+  const marginMode = watch("marginMode");
+  const [modalVisible, setModalVisible] = useState(false);
+  const { selectedExchange } = useStoreSettingsSelector();
+  const showCross = selectedExchange.exchangeName.toLowerCase() === "bitmex";
+  const leverageEditable =
+    marginMode !== "cross" || selectedExchange.exchangeName.toLowerCase() !== "bitmex";
+
+  /**
+   * Leverage mode change handler.
+   *
+   * @param {React.ChangeEvent<HTMLInputElement>} event Change event.
+   * @returns {void}
+   */
+  const handleModeChange = (event) => {
+    setValue("marginMode", event.target.checked ? "cross" : "isolated");
+  };
+
+  return (
+    <Box className="marginControl">
+      <HelperLabel descriptionId="terminal.leverage.help" labelId="terminal.leverage" />
+
+      <Box alignItems="center" display="flex" flexDirection="row">
+        <Box
+          className="leverageButton"
+          display="flex"
+          flexDirection="column"
+          justifyContent="flex-end"
+        >
+          <Modal
+            onClose={() => setModalVisible(false)}
+            persist={false}
+            size="small"
+            state={modalVisible}
+          >
+            <LeverageForm
+              leverage={parseInt(leverage)}
+              max={symbolData.maxLeverage}
+              min={1}
+              onClose={() => {
+                setModalVisible(false);
+              }}
+              setValue={setValue}
+            />
+          </Modal>
+          {leverageEditable && (
+            <Button onClick={() => setModalVisible(true)}>
+              {marginMode === "cross"
+                ? selectedExchange.exchangeName.toLowerCase() === "bitmex"
+                  ? "Cross"
+                  : `Cross ${leverage}x`
+                : `${leverage}x`}
+            </Button>
+          )}
+          <input name="leverage" ref={register} type="hidden" />
+          <input name="marginMode" ref={register} type="hidden" />
+        </Box>
+        {showCross && (
+          <Box alignItems="center" className="mode" display="flex" flexDirection="row">
+            <Typography className="callout1">
+              <FormattedMessage id="terminal.leverage.isolated" />
+            </Typography>
+            <Switch checked={marginMode === "cross"} onChange={handleModeChange} />
+            <Typography className="callout1">
+              <FormattedMessage id="terminal.leverage.cross" />
+            </Typography>
+          </Box>
+        )}
+      </Box>
+    </Box>
+  );
+};
+
+export default MarginControl;
--- src/components/TradingTerminal/LeverageForm/LeverageForm.js
+++ src/components/TradingTerminal/LeverageForm/LeverageForm.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Box, Slider, Typography, Switch } from "@material-ui/core";
+import { Box, Slider, Typography } from "@material-ui/core";
 import { FormattedMessage } from "react-intl";
 import "./LeverageForm.scss";
 import useStoreSettingsSelector from "../../../hooks/useStoreSettingsSelector";
@@ -10,7 +10,6 @@ import CustomButton from "../../CustomButton";
  * @property {number} min Minimum leverage limit.
  * @property {number} max Maximum leverage limit.
  * @property {number} leverage Current leverage.
- * @property {string} marginMode Current margin mode.
  * @property {function} setValue Hook form setValue callback.
  * @property {function} onClose Hook form setValue callback.
  */
@@ -22,9 +21,8 @@ import CustomButton from "../../CustomButton";
  * @returns {JSX.Element} Leverage form element.
  */
 const LeverageForm = (props) => {
-  const { min, max, leverage, setValue, marginMode: initMode, onClose } = props;
+  const { min, max, leverage, setValue, onClose } = props;
   const [val, setVal] = useState(leverage);
-  const [mode, setMode] = useState(initMode);
   const marks = [
     { value: 1, label: "1" },
     { value: 25, label: "25" },
@@ -33,18 +31,14 @@ const LeverageForm = (props) => {
     { value: 100, label: "100" },
     { value: 125, label: "125" },
   ];
-  const { darkStyle, selectedExchange } = useStoreSettingsSelector();
-  const leverageEditable =
-    mode !== "cross" || selectedExchange.exchangeName.toLowerCase() !== "bitmex";
-  const showCross = selectedExchange.exchangeName.toLowerCase() === "bitmex";
+  const { darkStyle } = useStoreSettingsSelector();
 
   const handleCancel = () => {
     onClose();
   };
 
   const handleConfirm = () => {
-    setValue("marginMode", mode);
-    setValue("leverage", leverageEditable ? val : 1);
+    setValue("leverage", val);
     onClose();
   };
 
@@ -92,16 +86,6 @@ const LeverageForm = (props) => {
     }
   };
 
-  /**
-   * Leverage mode change handler.
-   *
-   * @param {React.ChangeEvent<HTMLInputElement>} event Change event.
-   * @returns {void}
-   */
-  const handleModeChange = (event) => {
-    setMode(event.target.checked ? "cross" : "isolated");
-  };
-
   return (
     <Box
       className="leverageForm"
@@ -112,63 +96,43 @@ const LeverageForm = (props) => {
       <Typography className="title" id="range-slider" variant="h3">
         <FormattedMessage id="terminal.leverage.adjust" />
       </Typography>
-      {showCross && (
-        <Box alignItems="center" className="mode" display="flex" flexDirection="row">
-          <Typography>
-            <FormattedMessage id="terminal.leverage.isolated" />
-          </Typography>
-          <Switch checked={mode === "cross"} onChange={handleModeChange} />
-          <Typography>
-            <FormattedMessage id="terminal.leverage.cross" />
-          </Typography>
-        </Box>
-      )}
-      {leverageEditable && (
-        <>
-          <Box
-            className="inputValue"
-            display="flex"
-            flexDirection="row"
-            justifyContent="space-between"
-          >
-            <button
-              className={darkStyle ? "dark" : "light"}
-              onClick={() => decreaseValue()}
-              type="button"
-            >
-              −
-            </button>
-            <input
-              className={darkStyle ? "dark" : "light"}
-              onBlur={protectLimits}
-              onChange={handleInputChange}
-              value={val}
-            />
-            <button
-              className={darkStyle ? "dark" : "light"}
-              onClick={() => increaseValue()}
-              type="button"
-            >
-              +
-            </button>
-          </Box>
-          <Slider
-            aria-labelledby="range-slider"
-            className="slider"
-            classes={{ mark: "mark", thumb: "thumb", track: "track", markLabel: "markLabel" }}
-            marks={marks}
-            max={max}
-            min={min}
-            onChange={handleSliderChange}
-            step={1}
-            value={val}
-          />
-          {max > 25 && val >= 25 && (
-            <span className="errorText">
-              <FormattedMessage id="terminal.leverage.alert" />
-            </span>
-          )}
-        </>
+      <Box className="inputValue" display="flex" flexDirection="row" justifyContent="space-between">
+        <button
+          className={darkStyle ? "dark" : "light"}
+          onClick={() => decreaseValue()}
+          type="button"
+        >
+          −
+        </button>
+        <input
+          className={darkStyle ? "dark" : "light"}
+          onBlur={protectLimits}
+          onChange={handleInputChange}
+          value={val}
+        />
+        <button
+          className={darkStyle ? "dark" : "light"}
+          onClick={() => increaseValue()}
+          type="button"
+        >
+          +
+        </button>
+      </Box>
+      <Slider
+        aria-labelledby="range-slider"
+        className="slider"
+        classes={{ mark: "mark", thumb: "thumb", track: "track", markLabel: "markLabel" }}
+        marks={marks}
+        max={max}
+        min={min}
+        onChange={handleSliderChange}
+        step={1}
+        value={val}
+      />
+      {max > 25 && val >= 25 && (
+        <span className="errorText">
+          <FormattedMessage id="terminal.leverage.alert" />
+        </span>
       )}
       <Box
         alignItems="center"
