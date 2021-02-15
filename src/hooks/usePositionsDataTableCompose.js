370845b65fa79d9b0b667bd567133ff6d91cb222
Christophe Wagner <chris.wagn22@gmail.com>
2021-02-15 12:25:20 +0100

    fix: hide position age for PS

--- src/hooks/usePositionsDataTableCompose.js
+++ src/hooks/usePositionsDataTableCompose.js
@@ -201,7 +201,7 @@ export function usePositionDataTableCompose(positions, confirmActionHandler, ope
    */
   function renderAge(dataIndex) {
     const position = positions[dataIndex];
-    return <>{position.age}</>;
+    return <>{position.age || "-"}</>;
   }
 
   /**
