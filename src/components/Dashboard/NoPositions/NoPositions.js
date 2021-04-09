4d167a6ecdc1917dd5e244b531bd5e4b29dc07e0
Murad Malik <muradmalik7@gmail.com>
2021-04-09 13:29:42 +0500

    feat: redirected to PS marketplace when there are no postions

--- src/components/Dashboard/NoPositions/NoPositions.js
+++ src/components/Dashboard/NoPositions/NoPositions.js
@@ -23,7 +23,7 @@ import { navigate } from "gatsby";
  */
 const NoPositions = ({ isProfile, type }) => {
   const redirect = () => {
-    navigate("/copyTraders");
+    navigate("/profitSharing");
   };
 
   const emptyMessage = () => {
