3c6fb32428592c83960add7350fdc88fb147ab61
Christophe Wagner <chris.wagn22@gmail.com>
2021-04-28 23:48:47 +0200

    fix: Fix lint errors

--- src/components/Forms/CopyTraderForm/CopyTraderForm.js
+++ src/components/Forms/CopyTraderForm/CopyTraderForm.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import "./CopyTraderForm.scss";
-import { Box, TextField, Typography, InputAdornment } from "@material-ui/core";
+import { Box, TextField, Typography } from "@material-ui/core";
 import CustomButton from "../../CustomButton/CustomButton";
 import { useForm, Controller } from "react-hook-form";
 import { FormattedMessage } from "react-intl";
