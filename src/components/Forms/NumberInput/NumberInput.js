5705d63e600298daae9736203593b9067101ff76
Christophe Wagner <chris.wagn22@gmail.com>
2021-04-28 23:45:55 +0200

    fix: Fix types

--- src/components/Forms/NumberInput/NumberInput.js
+++ src/components/Forms/NumberInput/NumberInput.js
@@ -1,7 +1,22 @@
 import React, { useState } from "react";
 import { Controller } from "react-hook-form";
 import { TextField, InputAdornment } from "@material-ui/core";
+/**
+ * @typedef {import('react-hook-form').Control} Control
+ */
 
+/**
+ * Control to input numbers, replacing commas by dots.
+ * @param {Object} props Props
+ * @param {Control} props.control React Hook Form control
+ * @param {string} props.quote Quote
+ * @param {string|number} props.defaultValue Default Value
+ * @param {string} props.placeholder Placeholder
+ * @param {boolean} props.error Error
+ * @param {string} props.name Name
+ * @param {Object} props.rules Rules
+ * @returns {JSX.Element} JSX
+ */
 const NumberInput = ({ control, quote, defaultValue, placeholder, error, name, rules }) => {
   const [allocated, setAllocated] = useState(defaultValue);
 
