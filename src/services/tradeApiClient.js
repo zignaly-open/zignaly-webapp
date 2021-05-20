d93430c87b4407bf97d64317ce8c6277c30869ab
Christophe Wagner <chris.wagn22@gmail.com>
2021-05-20 17:36:18 +0200

    chore: Remove unused files

--- src/services/tradeApiClient.js
+++ src/services/tradeApiClient.js
@@ -7,7 +7,6 @@ import {
   positionsResponseTransform,
   positionsShortResponseTransform,
   providersResponseTransform,
-  providersStatsResponseTransform,
   userBalanceResponseTransform,
   positionItemTransform,
   userEquityResponseTransform,
