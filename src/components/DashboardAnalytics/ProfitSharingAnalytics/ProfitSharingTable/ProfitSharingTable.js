5ba5c1e5bf5ee6d20b0ab35b69d8058735e3876d
Murad Malik <muradmalik7@gmail.com>
2021-05-21 02:47:12 +0500

    feat: new tooltips added in Ps analytics to reduce tickets

--- src/components/DashboardAnalytics/ProfitSharingAnalytics/ProfitSharingTable/ProfitSharingTable.js
+++ src/components/DashboardAnalytics/ProfitSharingAnalytics/ProfitSharingTable/ProfitSharingTable.js
@@ -5,7 +5,9 @@ import { formatDate } from "../../../../utils/format";
 import { formatPrice } from "../../../../utils/formatters";
 import "./ProfitSharingTable.scss";
 import AccountingFilter from "../AccountingFilter";
-import { Box } from "@material-ui/core";
+import { Box, Tooltip } from "@material-ui/core";
+import HelpIcon from "@material-ui/icons/Help";
+import { pnlRetainInfoUrl } from "utils/affiliateURLs";
 
 /**
  * @typedef {import("../../../../services/tradeApiClient.types").ProfitSharingBalanceEntry} ProfitSharingBalanceEntry
@@ -86,7 +88,29 @@ const ProfitSharingTable = ({ data }) => {
       name: "amount",
       label: "col.amount",
       options: {
-        customBodyRender: (amount) => formatPrice(amount),
+        customBodyRenderLite: (dataIndex) => {
+          const rowData = data[dataIndex];
+          const showTooltip = rowData.type.toLowerCase() === "pnl" && rowData.amount === 0;
+          return (
+            <Box alignItems="center" display="flex" flexDirection="row">
+              <span>{formatPrice(rowData.amount)}</span>
+              {showTooltip && (
+                <Tooltip
+                  interactive
+                  placement="top"
+                  title={
+                    <FormattedMessage
+                      id="profitsharing.types.pnl.tooltip"
+                      values={{ url: pnlRetainInfoUrl }}
+                    />
+                  }
+                >
+                  <HelpIcon className="helpIcon" />
+                </Tooltip>
+              )}
+            </Box>
+          );
+        },
       },
     },
   ];
