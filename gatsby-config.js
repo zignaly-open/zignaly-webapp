83e71b3fff4200a814cab60e35d879f9637b1bee
Christophe Wagner <chris.wagn22@gmail.com>
2021-07-22 16:46:53 +0200

    fix: Remove /app path suffix

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,9 +9,7 @@ process.env.GATSBY_BUILD_MODE = process.env.NODE_ENV || "dev";
 console.log("\nTRADE API URL:", process.env.GATSBY_TRADEAPI_URL, "\n\n");
 
 module.exports = {
-  // This only works on prod build so currently is not possible to use app path
-  // on develop mode, see issue: https://github.com/gatsbyjs/gatsby/issues/3721
-  pathPrefix: "/app",
+  pathPrefix: "/",
   siteMetadata: {
     title: "Zignaly Gatsby Starter",
     description: "Zignaly the copy-trader platform.",
