1cb788a479d436570291f6660b84bb2d60c00476
Christophe Wagner <chris.wagn22@gmail.com>
2021-07-28 11:08:00 +0200

    fix: Restore path prefix

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,7 +11,9 @@ process.env.GATSBY_BUILD_MODE = process.env.NODE_ENV || "dev";
 console.log("\nTRADE API URL:", process.env.GATSBY_TRADEAPI_URL, "\n\n");
 
 module.exports = {
-  pathPrefix: "/",
+  // This only works on prod build so currently is not possible to use app path
+  // on develop mode, see issue: https://github.com/gatsbyjs/gatsby/issues/3721
+  pathPrefix: "/app",
   siteMetadata: {
     title: "Zignaly Gatsby Starter",
     description: "Zignaly the copy-trader platform.",
