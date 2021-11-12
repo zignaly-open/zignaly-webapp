6460a1f03f319ecbf681c602ad1afba46b8ff343
Christophe Wagner <chris.wagn22@gmail.com>
2021-11-12 17:20:54 +0100

    fix: Add btc network icon

--- src/utils/chain.ts
+++ src/utils/chain.ts
@@ -1,11 +1,15 @@
 import ETHIcon from "images/wallet/eth.svg";
 import BSCIcon from "images/wallet/bsc.svg";
+import BTCIcon from "images/wallet/btc.svg";
 
 export const getChainIcon = (chain: string) => {
   switch (chain) {
     case "BSC":
     case "BEP20 (BSC)":
       return BSCIcon;
+    case "Bitcoin":
+    case "BTC":
+      return BTCIcon;
     default:
       return ETHIcon;
   }
