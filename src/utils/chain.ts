6bb89b7c56852b405d37a71359204a556cac99c0
Christophe Wagner <chris.wagn22@gmail.com>
2022-02-15 18:45:45 +0700

    fix: Fix chains icon

--- src/utils/chain.ts
+++ src/utils/chain.ts
@@ -2,6 +2,8 @@ import ETHIcon from "images/wallet/eth.svg";
 import BSCIcon from "images/wallet/bsc.svg";
 import BTCIcon from "images/coins/btc.svg";
 import MATICIcon from "images/coins/matic.svg";
+import SOLIcon from "images/wallet/sol.svg";
+import TRXIcon from "images/wallet/trx.svg";
 
 export const getChainIcon = (chain: string) => {
   switch (chain.toUpperCase()) {
@@ -16,6 +18,10 @@ export const getChainIcon = (chain: string) => {
       return BTCIcon;
     case "MATIC":
       return MATICIcon;
+    case "SOL":
+      return SOLIcon;
+    case "TRX":
+      return TRXIcon;
     default:
       return ETHIcon;
   }
