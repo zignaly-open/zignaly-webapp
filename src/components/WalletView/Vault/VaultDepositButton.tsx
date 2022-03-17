56e44f319cbb203ccd7cee78b8aff19d875c1a27
Christophe Wagner <chris.wagn22@gmail.com>
2022-03-17 18:19:44 +0700

    feat: Adapt holder button

--- src/components/WalletView/Vault/VaultDepositButton.tsx
+++ src/components/WalletView/Vault/VaultDepositButton.tsx
@@ -24,7 +24,7 @@ const ActivatedButton = styled.div`
   align-items: center;
   font-size: 11px;
   font-weight: 600;
-  text-transform: uppercase;
+  text-transform: capitalize;
   color: ${({ theme }) => (theme.palette.type === "dark" ? "#B1F7CA" : "#36373f")};
   max-width: 210px;
   span {
@@ -33,15 +33,18 @@ const ActivatedButton = styled.div`
 `;
 
 const ActivatedButtonPledge = styled(ActivatedButton)`
-  text-transform: capitalize;
-  padding: 5px 5px;
+  padding: 5px 7px;
   flex-wrap: wrap;
-  ${({ active = true, theme }: { active: boolean }) =>
+  ${({ active, theme }: { active?: boolean }) =>
     !active &&
     css`
       background: rgba(48, 122, 233, 0.28);
       color: ${theme.palette.type === "dark" ? "#bec5fd" : "#3244d3"};
     `}
+`;
+
+const InactiveButton = styled(ActivatedButtonPledge)`
+  white-space: nowrap;
 `;
 
 interface VaultDepositButtonProps {
@@ -51,7 +54,12 @@ interface VaultDepositButtonProps {
   depositEnabled: boolean;
 }
 
-const VaultDepositButton = ({ balance, vault, depositEnabled, onClick }: VaultDepositButtonProps) => {
+const VaultDepositButton = ({
+  balance,
+  vault,
+  depositEnabled,
+  onClick,
+}: VaultDepositButtonProps) => {
   return balance >= vault.minBalance ? (
     <ActivatedButton>
       <FormattedMessage id="vault.activated" />
@@ -61,10 +69,12 @@ const VaultDepositButton = ({ balance, vault, depositEnabled, onClick }: VaultDe
       <FormattedMessage id="accounts.deposit" />
       &nbsp;
       <NumberFormat value={vault.minBalance} displayType="text" suffix={` ${vault.coin}`} />
-    </ButtonStyled >
-  ) : <ButtonStyled className="textPurple borderPurple">
-    <FormattedMessage id="vault.holders" values={{ coin: vault.coin }} />
-  </ButtonStyled >;
+    </ButtonStyled>
+  ) : (
+    <InactiveButton>
+      <FormattedMessage id="vault.holders" values={{ coin: vault.coin }} />
+    </InactiveButton>
+  );
 };
 
 export default VaultDepositButton;
