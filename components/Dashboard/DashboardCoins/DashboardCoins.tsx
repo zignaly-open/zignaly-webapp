adc2dd97f64ca2d695117d8bcb59844a3ce81b90
Christophe Wagner <chris.wagn22@gmail.com>
2022-04-21 14:49:23 +0700

    fix: BTC unit

--- components/Dashboard/DashboardCoins/DashboardCoins.tsx
+++ components/Dashboard/DashboardCoins/DashboardCoins.tsx
@@ -18,93 +18,86 @@ const DashboardCoins = () => {
   const { data: assets, error } = useExchangeAssets(selectedExchange?.internalId, true);
   const dispatch = useDispatch();
 
-  const columns = useMemo(
-    () => [
-      {
-        Header: intl.formatMessage({ id: "col.coin" }),
-        accessor: "coin",
-      },
-      {
-        Header: intl.formatMessage({ id: "col.coins.total" }),
-        accessor: "totalBalance",
-      },
-      {
-        Header: intl.formatMessage({ id: "col.coins.available" }),
-        accessor: "availableBalance",
-      },
-      {
-        Header: intl.formatMessage({ id: "col.coins.locked" }),
-        accessor: "lockedBalance",
-      },
-      {
-        Header: intl.formatMessage({ id: "col.totalValueBTC" }),
-        accessor: "totalBTC",
-      },
-      {
-        Header: intl.formatMessage({ id: "col.totalValueUSD" }),
-        accessor: "totalUSD",
-      },
-    ],
-    [],
-  );
+  const columns = [
+    {
+      Header: intl.formatMessage({ id: "col.coin" }),
+      accessor: "coin",
+    },
+    {
+      Header: intl.formatMessage({ id: "col.coins.total" }),
+      accessor: "totalBalance",
+    },
+    {
+      Header: intl.formatMessage({ id: "col.coins.available" }),
+      accessor: "availableBalance",
+    },
+    {
+      Header: intl.formatMessage({ id: "col.coins.locked" }),
+      accessor: "lockedBalance",
+    },
+    {
+      Header: intl.formatMessage({ id: "col.totalValueBTC" }),
+      accessor: "totalBTC",
+    },
+    {
+      Header: intl.formatMessage({ id: "col.totalValueUSD" }),
+      accessor: "totalUSD",
+    },
+  ];
 
-  const data = useMemo(
-    () =>
-      assets
-        ? Object.entries(assets)
-            // todo: remove once sorting fixed
-            .sort((a, b) => parseFloat(b[1].balanceTotalUSDT) - parseFloat(a[1].balanceTotalUSDT))
-            .map(([coin, coinBalance]) => ({
-              coin: (
-                <styled.CoinCell>
-                  <CoinIcon coin={coin} />
-                  <Box display="flex" flexDirection="column" ml="12px" alignItems="flex-start">
-                    <Typography style={{ lineHeight: "20px" }}>{coin}</Typography>
-                    <styled.TypographySecondary>{coinBalance.name}</styled.TypographySecondary>
-                  </Box>
-                </styled.CoinCell>
-              ),
-              totalBalance: <PriceLabel token={coin} value={coinBalance.balanceTotal} />,
-              availableBalance: <PriceLabel token={coin} value={coinBalance.balanceFree} />,
-              lockedBalance: <PriceLabel token={coin} value={coinBalance.balanceLocked} />,
-              totalBTC: <PriceLabel token={coin} value={coinBalance.balanceTotalBTC} />,
-              totalUSD: (
-                <NumberFormat
-                  value={coinBalance.balanceTotalUSDT}
-                  displayType="text"
-                  thousandSeparator={true}
-                  prefix="$"
-                  decimalScale={2}
-                />
-              ),
-              action: (
-                <ButtonGroup>
-                  <TableButton
-                    caption={intl.formatMessage({ id: "action.deposit" })}
-                    onClick={() =>
-                      dispatch(
-                        openModal(ModalTypesId.DEPOSIT_MODAL, {
-                          initialCoin: coin,
-                        }),
-                      )
-                    }
-                  />
-                  <TableButton
-                    caption={intl.formatMessage({ id: "action.withdraw" })}
-                    onClick={() =>
-                      dispatch(
-                        openModal(ModalTypesId.WITHDRAW_MODAL, {
-                          initialCoin: coin,
-                        }),
-                      )
-                    }
-                  />
-                </ButtonGroup>
-              ),
-            }))
-        : undefined,
-    [assets],
-  );
+  const data = assets
+    ? Object.entries(assets)
+        // todo: remove once sorting fixed
+        .sort((a, b) => parseFloat(b[1].balanceTotalUSDT) - parseFloat(a[1].balanceTotalUSDT))
+        .map(([coin, coinBalance]) => ({
+          coin: (
+            <styled.CoinCell>
+              <CoinIcon coin={coin} />
+              <Box display="flex" flexDirection="column" ml="12px" alignItems="flex-start">
+                <Typography style={{ lineHeight: "20px" }}>{coin}</Typography>
+                <styled.TypographySecondary>{coinBalance.name}</styled.TypographySecondary>
+              </Box>
+            </styled.CoinCell>
+          ),
+          totalBalance: <PriceLabel token={coin} value={coinBalance.balanceTotal} />,
+          availableBalance: <PriceLabel token={coin} value={coinBalance.balanceFree} />,
+          lockedBalance: <PriceLabel token={coin} value={coinBalance.balanceLocked} />,
+          totalBTC: <PriceLabel token="BTC" value={coinBalance.balanceTotalBTC} />,
+          totalUSD: (
+            <NumberFormat
+              value={coinBalance.balanceTotalUSDT}
+              displayType="text"
+              thousandSeparator={true}
+              prefix="$"
+              decimalScale={2}
+            />
+          ),
+          action: (
+            <ButtonGroup>
+              <TableButton
+                caption={intl.formatMessage({ id: "action.deposit" })}
+                onClick={() =>
+                  dispatch(
+                    openModal(ModalTypesId.DEPOSIT_MODAL, {
+                      initialCoin: coin,
+                    }),
+                  )
+                }
+              />
+              <TableButton
+                caption={intl.formatMessage({ id: "action.withdraw" })}
+                onClick={() =>
+                  dispatch(
+                    openModal(ModalTypesId.WITHDRAW_MODAL, {
+                      initialCoin: coin,
+                    }),
+                  )
+                }
+              />
+            </ButtonGroup>
+          ),
+        }))
+    : undefined;
 
   return <>{data ? <Table columns={columns} data={data} /> : <Loader />}</>;
 };
