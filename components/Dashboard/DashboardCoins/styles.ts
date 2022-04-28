7b99d0c8b2bac0f434db8350370079153e5e355e
Christophe Wagner <chris.wagn22@gmail.com>
2022-04-28 22:06:03 +0700

    feat: Migrate to CoinLabel

--- components/Dashboard/DashboardCoins/styles.ts
+++ components/Dashboard/DashboardCoins/styles.ts
@@ -1,16 +1,2 @@
 import styled from "styled-components";
 import { Typography } from "zignaly-ui";
-
-export const CoinCell = styled.div`
-  display: flex;
-  margin-left: 42px;
-  align-items: center;
-  justify-content: flex-start;
-`;
-
-export const TypographySecondary = styled(Typography)`
-  font-weight: 600;
-  font-size: 12px;
-  color: #706f82;
-  margin-top: 4px;
-`;
