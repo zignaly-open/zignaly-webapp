29ba627fed7da8bb775388fad32d4d4e28fd7c71
Christophe Wagner <chris.wagn22@gmail.com>
2022-10-25 12:31:33 +0200

    fix: Use old signup page

--- src/pages/signup/index.js
+++ src/pages/signup/index.js
@@ -3,7 +3,7 @@ import "./signup.scss";
 import LoginTabs from "../../components/Login/LoginTabs";
 import { Helmet } from "react-helmet";
 import { useIntl } from "react-intl";
-import SignupForm from "../../components/Forms/SignupForm";
+import SignupForm from "../../components/Forms/SignupForm/SignupForm";
 import SignupFormB from "../../components/Forms/SignupForm/SignupFormB";
 import useRedirectUponSessionValid from "../../hooks/useRedirectUponSessionValid";
 import Login from "../../components/Login/Login";
